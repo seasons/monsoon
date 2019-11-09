38c4bcf8df45b25cdaacc3dde0001c50f56b0521
Luc Succes <l2succes@gmail.com>
2019-11-09 14:11:50 -0500

    Adds customer info to airtable

--- src/airtable/createUser.ts
+++ src/airtable/createUser.ts
@@ -1,14 +1,40 @@
 import { base } from "./config"
+import { CustomerDetailCreateInput } from "../prisma"
 
-export const createUser = (user, details) => {
+export const createUser = (user, details: CustomerDetailCreateInput) => {
   const { email, firstName, lastName } = user
   const { phoneNumber } = details
+
+  const keyMap = {
+    phoneNumber: "Phone Number",
+    birthday: "Birthday",
+    height: "Height",
+    weight: "Weight",
+    bodyType: "Body Type",
+    averageTopSize: "Average Top Size",
+    averageWaistSize: "Average Waist Size",
+    averagePantLength: "Average Pant Length",
+    preferredPronouns: "Preferred Pronouns",
+    profession: "Profession",
+    partyFrequency: "Party Frequency",
+    travelFrequency: "Travel Frequency",
+    shoppingFrequency: "Shopping Frequency",
+    averageSpend: "Average Spend",
+    style: "Style",
+    commuteStyle: "Commute Style",
+    shippingAddress: "Shipping Address",
+    phoneOS: "Phone OS",
+  }
 
   const data = {
     Email: email,
     "First Name": firstName,
     "Last Name": lastName,
     "Phone Number": phoneNumber,
+  }
+
+  for (let key in details) {
+    data[keyMap[key]] = details[key]
   }
 
   base("Users").create([
