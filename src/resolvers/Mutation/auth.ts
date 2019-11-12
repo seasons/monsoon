45bc13db4a2cdf49cf35483780504678ef28a3c2
frahman5 <faiyam@faiyamrahman.com>
2019-11-12 15:14:24 -0500

    adds processCompletedChargebeeHostedCheckout mutation

--- src/resolvers/Mutation/auth.ts
+++ src/resolvers/Mutation/auth.ts
@@ -1,104 +1,104 @@
 import {
-  createPrismaCustomerForExistingUser,
-  createAuth0User,
-  createPrismaUser,
-  getAuth0UserAccessToken,
-  isLoggedIn,
+    createPrismaCustomerForExistingUser,
+    createAuth0User,
+    createPrismaUser,
+    getAuth0UserAccessToken,
+    isLoggedIn,
 } from "../../auth/utils"
 import { Context } from "../../utils"
 import { UserInputError, ForbiddenError } from "apollo-server"
 
 export const auth = {
-  // The signup mutation signs up users with a "Customer" role.
-  async signup(
-    obj,
-    { email, password, firstName, lastName, details },
-    ctx: Context,
-    info
-  ) {
-    // Register the user on Auth0
-    let userAuth0ID
-    try {
-      userAuth0ID = await createAuth0User(email, password, {
-        firstName,
-        lastName,
-      })
-    } catch (err) {
-      if (err.message.includes("400")) {
-        throw new UserInputError(err)
-      }
-      throw new Error(err)
-    }
+    // The signup mutation signs up users with a "Customer" role.
+    async signup(
+        obj,
+        { email, password, firstName, lastName, details },
+        ctx: Context,
+        info
+    ) {
+        // Register the user on Auth0
+        let userAuth0ID
+        try {
+            userAuth0ID = await createAuth0User(email, password, {
+                firstName,
+                lastName,
+            })
+        } catch (err) {
+            if (err.message.includes("400")) {
+                throw new UserInputError(err)
+            }
+            throw new Error(err)
+        }
 
-    // Get their API access token
-    let tokenData
-    try {
-      tokenData = await getAuth0UserAccessToken(email, password)
-    } catch (err) {
-      if (err.message.includes("403")) {
-        throw new ForbiddenError(err)
-      }
-      throw new UserInputError(err)
-    }
+        // Get their API access token
+        let tokenData
+        try {
+            tokenData = await getAuth0UserAccessToken(email, password)
+        } catch (err) {
+            if (err.message.includes("403")) {
+                throw new ForbiddenError(err)
+            }
+            throw new UserInputError(err)
+        }
 
-    // Create a user object in our database
-    let user
-    try {
-      user = await createPrismaUser(ctx, {
-        auth0Id: userAuth0ID,
-        email,
-        firstName,
-        lastName,
-      })
-    } catch (err) {
-      throw new Error(err)
-    }
+        // Create a user object in our database
+        let user
+        try {
+            user = await createPrismaUser(ctx, {
+                auth0Id: userAuth0ID,
+                email,
+                firstName,
+                lastName,
+            })
+        } catch (err) {
+            throw new Error(err)
+        }
 
-    // Create a customer object in our database
-    let customer
-    try {
-      customer = await createPrismaCustomerForExistingUser(ctx, {
-        userID: user.id,
-        details,
-        status: "Waitlisted",
-      })
-    } catch (err) {
-      throw new Error(err)
-    }
+        // Create a customer object in our database
+        let customer
+        try {
+            customer = await createPrismaCustomerForExistingUser(ctx, {
+                userID: user.id,
+                details,
+                status: "Waitlisted",
+            })
+        } catch (err) {
+            throw new Error(err)
+        }
 
-    return {
-      token: tokenData.access_token,
-      refreshToken: tokenData.refresh_token,
-      expiresIn: tokenData.expires_in,
-      user: user,
-    }
-  },
+        return {
+            token: tokenData.access_token,
+            refreshToken: tokenData.refresh_token,
+            expiresIn: tokenData.expires_in,
+            user: user,
+        }
+    },
 
-  async login(obj, { email, password }, ctx: Context, info) {
-    // If they are already logged in, throw an error
-    if (isLoggedIn(ctx)) {
-      throw new Error(`user is already logged in`)
-    }
+    async login(obj, { email, password }, ctx: Context, info) {
+        // If they are already logged in, throw an error
+        if (isLoggedIn(ctx)) {
+            throw new Error(`user is already logged in`)
+        }
 
-    // Get their API access token
-    let tokenData
-    try {
-      tokenData = await getAuth0UserAccessToken(email, password)
-    } catch (err) {
-      if (err.message.includes("403")) {
-        throw new ForbiddenError(err)
-      }
-      throw new UserInputError(err)
-    }
+        // Get their API access token
+        let tokenData
+        try {
+            tokenData = await getAuth0UserAccessToken(email, password)
+        } catch (err) {
+            if (err.message.includes("403")) {
+                throw new ForbiddenError(err)
+            }
+            throw new UserInputError(err)
+        }
 
-    // Get user with this email
-    let user = await ctx.prisma.user({ email })
+        // Get user with this email
+        let user = await ctx.prisma.user({ email })
 
-    return {
-      token: tokenData.access_token,
-      refreshToken: tokenData.refresh_token,
-      expiresIn: tokenData.expires_in,
-      user,
-    }
-  },
+        return {
+            token: tokenData.access_token,
+            refreshToken: tokenData.refresh_token,
+            expiresIn: tokenData.expires_in,
+            user,
+        }
+    },
 }
--- src/resolvers/Mutation/customer.ts
+++ src/resolvers/Mutation/customer.ts
@@ -1,132 +1,152 @@
-import { Context } from "../../utils"
+import { Context, getUserFromUserIDHash, getCustomerFromUserID, setCustomerPrismaStatus } from "../../utils"
 import {
-  getCustomerFromContext,
-  getUserFromContext,
-  getUserId,
+    getCustomerFromContext,
+    getUserFromContext,
+    getUserId,
 } from "../../auth/utils"
 import { UserInputError } from "apollo-server"
 import chargebee from "chargebee"
-import { createOrUpdateAirtableUser } from "../../airtable/createUser"
+import { createOrUpdateAirtableUser } from "../../airtable/createOrUpdateUser"
+import { getUserById } from "../../airtable/utils"
+import { base } from "../../airtable/config"
 
 export const customer = {
-  /*
-    Test Cases for addCustomerDetails resolver:
-    IF user submits an 'id' field in the resolver inputs, they get an error. 
-
-    IF there is no value for the Authorization header, THEN they get a 401. 
-
-    IF they use a token for an admin, THEN they get a 401
-
-    IF they use a token for a partner, THEN they get a 401
-
-    IF they add details to a customer with no existing customerDetail record, 
-    THEN a new record is created 
-    AND the customer record's "detail" field has the id of the newly created customerDetail record
-
-    IF they add details to a customer with an existing details object
-    THEN the existing record is updated, with any fields that were previously
-    written to overwritten if the payload includes values for them. 
-    */
-  async addCustomerDetails(obj, { details }, ctx: Context, info) {
-    // They should not have included any "id" in the input
-    if (details.id != null) {
-      throw new UserInputError("payload should not include id")
-    }
-
-    // Grab the customer off the context
-    const user = await getUserFromContext(ctx)
-    const customer = await getCustomerFromContext(ctx)
-
-    // Add the details. If necessary, create the details object afresh.
-    let updatedDetails
-    let currentCustomerDetail = await ctx.prisma
-      .customer({ id: customer.id })
-      .detail()
-    if (currentCustomerDetail == null) {
-      await ctx.prisma.updateCustomer({
-        data: { detail: { create: details } },
-        where: { id: customer.id },
-      })
-      updatedDetails = await ctx.prisma.customer({ id: customer.id }).detail()
-    } else {
-      updatedDetails = await ctx.prisma.updateCustomerDetail({
-        data: details,
-        where: { id: currentCustomerDetail.id },
-      })
-    }
-
-    await createOrUpdateAirtableUser(user, {
-      ...currentCustomerDetail,
-      ...details,
-    })
-
-    // Return the updated customer object
-    return { ...customer, detail: updatedDetails }
-  },
-
-  async saveCustomerBillingInfo(obj, args, ctx: Context, info) {
-    // Get the customer's id
-    const { id } = await getUserId(ctx)
-    const prismaCustomer = await getCustomerFromContext(ctx)
-
-    // Retrieve all the relevant data
-    chargebee.configure({
-      site: process.env.CHARGEBEE_SITE,
-      api_key: process.env.CHARGEE_API_KEY,
-    })
-    let billingInfo, plan, planInfo
-    await chargebee.subscription
-      .list({
-        limit: 1,
-        "customer_id[is]": id,
-      })
-      .request(async function(error, result) {
-        if (error) {
-          throw new Error(error)
-        } else {
-          const subscription = result.list[0].subscription
-          const chargebeeCustomer = result.list[0].customer
-          const card = result.list[0].card
-
-          // Store all the relevant data
-          if (subscription.plan_id == "essential") {
-            plan = "Essential"
-          } else if (subscription.plan_id == "all-access") {
-            plan = "AllAccess"
-          } else {
-            throw new Error(`unexpected plan-id: ${subscription.plan_id}`)
-          }
-          billingInfo = {
-            brand: card.card_type,
-            name: `${card.first_name} ${card.last_name}`,
-            last_digits: card.last4,
-            expiration_month: card.expiry_month,
-            expiration_year: card.expiry_year,
-            street1: chargebeeCustomer.billing_address.line1,
-            street2: chargebeeCustomer.billing_address.line2,
-            city: chargebeeCustomer.billing_address.city,
-            state: chargebeeCustomer.billing_address.state,
-            country: chargebeeCustomer.billing_address.country,
-            postal_code: chargebeeCustomer.billing_address.zip,
-          }
-          await ctx.prisma.updateCustomer({
-            data: {
-              plan: plan,
-              billingInfo: {
-                upsert: {
-                  create: billingInfo,
-                  update: billingInfo,
-                },
-              },
-            },
-            where: { id: prismaCustomer.id },
-          })
+    /*
+      Test Cases for addCustomerDetails resolver:
+      IF user submits an 'id' field in the resolver inputs, they get an error. 
+  
+      IF there is no value for the Authorization header, THEN they get a 401. 
+  
+      IF they use a token for an admin, THEN they get a 401
+  
+      IF they use a token for a partner, THEN they get a 401
+  
+      IF they add details to a customer with no existing customerDetail record, 
+      THEN a new record is created 
+      AND the customer record's "detail" field has the id of the newly created customerDetail record
+  
+      IF they add details to a customer with an existing details object
+      THEN the existing record is updated, with any fields that were previously
+      written to overwritten if the payload includes values for them. 
+      */
+    async addCustomerDetails(obj, { details }, ctx: Context, info) {
+        // They should not have included any "id" in the input
+        if (details.id != null) {
+            throw new UserInputError("payload should not include id")
         }
-      })
 
-    return {
-      billingInfo: ctx.prisma.customer({ id: prismaCustomer.id }).billingInfo(),
-      plan: ctx.prisma.customer({ id: prismaCustomer.id }).plan(),
-    }
-  },
+        // Grab the customer off the context
+        const user = await getUserFromContext(ctx)
+        const customer = await getCustomerFromContext(ctx)
+
+        // Add the details. If necessary, create the details object afresh.
+        let updatedDetails
+        let currentCustomerDetail = await ctx.prisma
+            .customer({ id: customer.id })
+            .detail()
+        if (currentCustomerDetail == null) {
+            await ctx.prisma.updateCustomer({
+                data: { detail: { create: details } },
+                where: { id: customer.id },
+            })
+            updatedDetails = await ctx.prisma.customer({ id: customer.id }).detail()
+        } else {
+            updatedDetails = await ctx.prisma.updateCustomerDetail({
+                data: details,
+                where: { id: currentCustomerDetail.id },
+            })
+        }
+
+        await createOrUpdateAirtableUser(user, {
+            ...currentCustomerDetail,
+            ...details,
+        })
+
+        // Return the updated customer object
+        return { ...customer, detail: updatedDetails }
+    },
+
+    async processCompletedChargebeeHostedCheckout(obj, { hostedPageID }, ctx: Context, info) {
+        chargebee.configure({
+            site: process.env.CHARGEBEE_SITE,
+            api_key: process.env.CHARGEE_API_KEY,
+        })
+        let prismaUser
+        await chargebee.hosted_page.acknowledge(hostedPageID).request(async function (error, result) {
+            if (error) {
+                console.log(error)
+            } else {
+                var { subscription: { customer_id } } = result.hosted_page.content;
+                prismaUser = await ctx.prisma.user({ id: customer_id })
+                await setCustomerPrismaStatus(ctx.prisma, prismaUser, "Active")
+                await createOrUpdateAirtableUser(prismaUser, {}, "Active")
+            }
+        })
+    },
+
+    async saveCustomerBillingInfo(obj, args, ctx: Context, info) {
+        // Get the customer's id
+        const { id } = await getUserId(ctx)
+        const prismaCustomer = await getCustomerFromContext(ctx)
+
+        // Retrieve all the relevant data
+        chargebee.configure({
+            site: process.env.CHARGEBEE_SITE,
+            api_key: process.env.CHARGEE_API_KEY,
+        })
+        let billingInfo, plan, planInfo
+        await chargebee.subscription
+            .list({
+                limit: 1,
+                "customer_id[is]": id,
+            })
+            .request(async function (error, result) {
+                if (error) {
+                    throw new Error(error)
+                } else {
+                    const subscription = result.list[0].subscription
+                    const chargebeeCustomer = result.list[0].customer
+                    const card = result.list[0].card
+
+                    // Store all the relevant data
+                    if (subscription.plan_id == "essential") {
+                        plan = "Essential"
+                    } else if (subscription.plan_id == "all-access") {
+                        plan = "AllAccess"
+                    } else {
+                        throw new Error(`unexpected plan-id: ${subscription.plan_id}`)
+                    }
+                    billingInfo = {
+                        brand: card.card_type,
+                        name: `${card.first_name} ${card.last_name}`,
+                        last_digits: card.last4,
+                        expiration_month: card.expiry_month,
+                        expiration_year: card.expiry_year,
+                        street1: chargebeeCustomer.billing_address.line1,
+                        street2: chargebeeCustomer.billing_address.line2,
+                        city: chargebeeCustomer.billing_address.city,
+                        state: chargebeeCustomer.billing_address.state,
+                        country: chargebeeCustomer.billing_address.country,
+                        postal_code: chargebeeCustomer.billing_address.zip,
+                    }
+                    await ctx.prisma.updateCustomer({
+                        data: {
+                            plan: plan,
+                            billingInfo: {
+                                upsert: {
+                                    create: billingInfo,
+                                    update: billingInfo,
+                                },
+                            },
+                        },
+                        where: { id: prismaCustomer.id },
+                    })
+                }
+            })
+
+        return {
+            billingInfo: ctx.prisma.customer({ id: prismaCustomer.id }).billingInfo(),
+            plan: ctx.prisma.customer({ id: prismaCustomer.id }).plan(),
+        }
+    },
 }
