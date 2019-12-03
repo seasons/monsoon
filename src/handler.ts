2f0b49d22b3e49ad8c68353a5a5178a3a8fe635f
frahman5 <faiyam@faiyamrahman.com>
2019-12-03 11:41:33 -0500

    removes function that handles completed reservations

--- src/handler.ts
+++ src/handler.ts
@@ -50,59 +50,11 @@ export async function checkAndAuthorizeUsers(event, context, callback) {
   return response
 }
 
-export async function completeReservations(event, context, callback) {
-  const updated = []
-  const reservationsInAirtableButNotPrisma = []
-  const allAirtableReservations = await getAllReservations()
-  for (let airtableReservation of allAirtableReservations) {
-    if (airtableReservation.fields.Status === "Completed") {
-      // TODO: update how we grab the reservation
-      const prismaReservation = await prisma.reservation({ id: "asdf09asdf" })
-
-      if (!!prismaReservation) {
-        if (prismaReservation.status !== "Completed") {
-          updated.push(prismaReservation.reservationNumber)
-          const prismaUser = await prisma.user({
-            email: airtableReservation.fields["User Email"],
-          })
-
-          // Update the status
-          prisma.updateReservation({
-            data: { status: "Completed" },
-            where: { id: prismaReservation.id },
-          })
-
-          // Email the user
-          sendYouCanNowReserveAgainEmail(prismaUser)
-
-          // Update the returnPackage on the shipment
-          // Check each item on the airtable reservation record. If it's reservable,
-          // add it the products on the returnPackage. Then calculate the full
-          // weight of the package and add that to the returnPackage as well.
-        }
-      } else {
-        reservationsInAirtableButNotPrisma.push(airtableReservation.fields.ID)
-      }
-    }
-  }
-
-  return {
-    updated,
-    reservationsInAirtableButNotPrisma,
-  }
-}
-
 function sendAuthorizedToSubscribeEmail(user: User) {
   sendTransactionalEmail(user.email, "d-a62e1c840166432abd396d1536e4489d", {
     name: user.firstName,
     url: `${process.env.SEEDLING_URL}/complete?idHash=${getUserIDHash(
       user.id
     )}`,
-  })
-}
-
-function sendYouCanNowReserveAgainEmail(user: User) {
-  sendTransactionalEmail(user.email, "TK", {
-    name: user.firstName,
   })
 }
