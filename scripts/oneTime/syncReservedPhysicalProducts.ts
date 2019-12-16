9d00f9b0996833921807930a8a30f2bd2874c3a6
frahman5 <faiyam@faiyamrahman.com>
2019-12-16 18:08:32 -0500

    updates and runs sync reserved physical products script

--- scripts/oneTime/syncReservedPhysicalProducts.ts
+++ scripts/oneTime/syncReservedPhysicalProducts.ts
@@ -9,59 +9,51 @@ REVISITING THE LOGIC
 /*
 TODO:
 - Update this to also update status of physicalProduct on airtable 
-- Update this to more carefully assess which reservations are active or not
 */
 
 import { prisma } from "../../src/prisma"
-import { base } from "../../src/airtable/config"
 import { getAllPhysicalProducts } from "../../src/airtable/utils"
 import { updatePhysicalProduct } from "../../src/airtable/updatePhysicalProduct"
-import Sentry from "@sentry/node"
+import { getCorrespondingAirtablePhysicalProduct } from "../utils"
 
+const Sentry = require("@sentry/node")
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
 })
 
 async function syncReservedPhysicalProductStatuses() {
   const allActiveReservations = await prisma.reservations({
-    where: { status: "InQueue" },
+    where: { status_not_in: ["Completed", "Cancelled"] },
   })
   const allAirtablePhysicalProducts = await getAllPhysicalProducts()
 
   for (let resy of allActiveReservations) {
-    console.log(`\nUPDATING PRODUCTS FROM RESY ${resy.id}`)
+    console.log(`\nUPDATING PRODUCTS FROM RESY ${resy.reservationNumber}`)
 
     const physicalProducts = await prisma
       .reservation({ id: resy.id })
       .products()
-    console.log("BEFORE UPDATE")
-    console.log(physicalProducts)
 
-    for (let product of physicalProducts) {
+    for (let physProd of physicalProducts) {
       // Update its status in prisma
       await prisma.updatePhysicalProduct({
         data: { inventoryStatus: "Reserved" },
-        where: { id: product.id },
+        where: { id: physProd.id },
       })
+      console.log(`Update ${physProd.seasonsUID} status --> Reserved in prisma`)
 
       // Update status on airtable
-      base("Physical Products").find("recK20SrZ6Ifu2jYs", function(
-        err,
-        record
-      ) {
-        if (err) {
-          Sentry.captureException(err)
-          return
-        }
-        updatePhysicalProduct(record.id, { "Inventory Status": "Reserved" })
+      const correspondingAirtablePhysicalproduct = getCorrespondingAirtablePhysicalProduct(
+        allAirtablePhysicalProducts,
+        physProd
+      )
+      updatePhysicalProduct(correspondingAirtablePhysicalproduct.id, {
+        "Inventory Status": "Reserved",
       })
+      console.log(
+        `Update ${physProd.seasonsUID} status --> Reserved in airtable`
+      )
     }
-
-    const physicalProducts2 = await prisma
-      .reservation({ id: resy.id })
-      .products()
-    console.log("AFTER UPDATE")
-    console.log(physicalProducts2)
   }
 }
 
