8a06fc940462d0e5033dacc531e0f42aa9bd7264
Luc Succes <l2succes@gmail.com>
2019-12-17 13:12:44 -0500

    Hotfix

--- src/cron/syncPhysicalProductAndReservationStatus.ts
+++ src/cron/syncPhysicalProductAndReservationStatus.ts
@@ -11,10 +11,11 @@ export async function syncPhysicalProductAndReservationStatus(
   callback
 ) {
   const physProdReport = await syncPhysicalProductStatus()
-  const reservationReport = await syncReservationStatus()
-  const allErrors = [...physProdReport.errors, ...reservationReport.errors]
-  console.log({ ...physProdReport, ...reservationReport, errors: allErrors })
-  return { ...physProdReport, ...reservationReport, errors: allErrors }
+  return physProdReport
+  // const reservationReport = await syncReservationStatus()
+  // const allErrors = [...physProdReport.errors, ...reservationReport.errors]
+  // console.log({ ...physProdReport, ...reservationReport, errors: allErrors })
+  // return { ...physProdReport, ...reservationReport, errors: allErrors }
 }
 
 syncPhysicalProductAndReservationStatus(null, null, null)
