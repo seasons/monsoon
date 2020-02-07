5f373ede934c07ae638cf58dabd3f2188a0b690b
frahman5 <faiyam@faiyamrahman.com>
2020-02-07 17:09:31 -0500

    refactors welcome to seasons email to use master email template. tidies up data in emails file

--- src/emails.ts
+++ src/emails.ts
@@ -13,7 +13,9 @@ export const emails = {
               html: `Items returned: ${itemsReturned}<br></br>User: ${userEmail}}`,
             },
             {
-              html: `Please confirm that the reservation was processed as expected. If anything looks wrong, please alert a developer.`,
+              html:
+                `Please confirm that the reservation was processed as expected.` +
+                `If anything looks wrong, please alert a developer.`,
             },
           ],
         },
@@ -53,11 +55,14 @@ export const emails = {
         body: {
           paragraphs: [
             {
-              html: `Hey ${firstName}, thanks for waiting. We're ready for you. Tap the button below to choose your membership plan, get the beta app and start reserving.`,
+              html:
+                `Hey ${firstName}, thanks for waiting. We're ready for you. ` +
+                `Tap the button below to choose your membership plan, get the beta app and start reserving.`,
             },
             {
               html:
-                "Heads up -- due to demand, we may need to give your spot to someone else if you don't choose your plan within the next 48 hours.",
+                `Heads up -- due to demand, we may need to give your spot to someone` +
+                `else if you don't choose your plan within the next 48 hours.`,
             },
             { html: "Thanks,<br>The Seasons Team" },
           ],
@@ -82,7 +87,8 @@ export const emails = {
           paragraphs: [
             {
               html:
-                "Good news! Your items made it back to Seasons HQ and we've reset your bag. You can now place a new reservation",
+                `Good news! Your items made it back to Seasons HQ and we've reset your bag.` +
+                ` You can now place a new reservation`,
             },
             {
               html:
@@ -92,6 +98,41 @@ export const emails = {
         },
         title: "We've got your order",
         subject: "We've received your return",
+      },
+    }
+  },
+  welcomeToSeasonsData: firstName => {
+    return {
+      email: {
+        body: {
+          paragraphs: [
+            {
+              html:
+                `Hey ${firstName}, thank you so much for signing up. We're excited` +
+                `you're here. As a Founding Member, you'll be the first to know about` +
+                ` exclusive features, products and restocks.`,
+            },
+            {
+              html:
+                `We're still actively working on the beta app. If you have any ideas` +
+                ` or thoughts about the service, give us your feedback! Good or bad.` +
+                ` Just write to membership@seasons.com`,
+            },
+            {
+              html:
+                '<b style="color:#000000">How to install the app and reserve your first three pieces</b>',
+            },
+            {
+              html:
+                `<ol><li>View this e-mail on your iOS device.</li><li>If you don\'t` +
+                ` already have it, download the <a href="${process.env.TESTFLIGHT_URL}">testflight app</a>.` +
+                `</li><li><a href="${process.env.TESTFLIGHT_URL}">Click here</a> to install` +
+                ` the seasons app on testflight.</li><li>Login and start reserving!</li></ol>`,
+            },
+          ],
+        },
+        title: "Welcome to Seasons.",
+        subject: "Welcome to Seasons",
       },
     }
   },
