af7f7ac3111cbd1e9aff4f606508cc75d5ff5270
frahman5 <faiyam@faiyamrahman.com>
2020-02-21 15:06:10 -0500

    adds sentry and results reporting

--- src/cron/sendReturnNotifications.ts
+++ src/cron/sendReturnNotifications.ts
@@ -3,36 +3,71 @@ import { DateTime, Interval } from "luxon"
 import { sendTransactionalEmail } from "../sendTransactionalEmail"
 import { emails } from "../emails"
 import { formatReservationReturnDate } from "../resolvers/Product/formatReservationReturnDate"
+import * as Sentry from "@sentry/node"
+
+const shouldReportErrorsToSentry = process.env.NODE_ENV === "production"
+
+if (shouldReportErrorsToSentry) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+  })
+}
 
 export const sendReturnNotifications = async () => {
   const reservations = await prisma.reservations({
     orderBy: "createdAt_DESC",
   })
-
+  const report = {
+    reservationsForWhichRemindersWereSent: [],
+    errors: [],
+  }
   for (const reservation of reservations) {
-    if (await returnNoticeNeeded(reservation)) {
-      //   Remind customer to return items
-      const user = await prisma
-        .reservation({
-          id: reservation.id,
+    try {
+      if (shouldReportErrorsToSentry) {
+        Sentry.configureScope(scope => {
+          scope.setExtra("reservationNumber", reservation.reservationNumber)
+          scope.setExtra("reservation createdAt", reservation.createdAt)
+          scope.setExtra("reservation status", reservation.status)
         })
-        .customer()
-        .user()
-      sendTransactionalEmail({
-        to: user.email,
-        data: emails.returnReminderData({
-          name: user.firstName,
-          returnDate: formatReservationReturnDate(
-            new Date(reservation.createdAt)
-          ),
-        }),
-      })
-      await prisma.updateReservation({
-        where: { id: reservation.id },
-        data: { reminderSentAt: DateTime.local().toString() },
-      })
+      }
+      if (await returnNoticeNeeded(reservation)) {
+        //   Remind customer to return items
+        const user = await prisma
+          .reservation({
+            id: reservation.id,
+          })
+          .customer()
+          .user()
+
+        sendTransactionalEmail({
+          to: user.email,
+          data: emails.returnReminderData({
+            name: user.firstName,
+            returnDate: formatReservationReturnDate(
+              new Date(reservation.createdAt)
+            ),
+          }),
+        })
+
+        await prisma.updateReservation({
+          where: { id: reservation.id },
+          data: { reminderSentAt: DateTime.local().toString() },
+        })
+
+        report.reservationsForWhichRemindersWereSent.push(
+          reservation.reservationNumber
+        )
+      }
+    } catch (err) {
+      report.errors.push(err)
+      if (shouldReportErrorsToSentry) {
+        Sentry.captureException(err)
+      }
     }
   }
+
+  console.log(report)
+  return report
 }
 
 const returnNoticeNeeded = async (reservation: Reservation) => {
@@ -55,5 +90,3 @@ const returnNoticeNeeded = async (reservation: Reservation) => {
     !["Cancelled", "Completed"].includes(reservation.status)
   )
 }
-
-sendReturnNotifications()
