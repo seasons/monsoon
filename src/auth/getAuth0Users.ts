945e76015c2a04400a917473a07d0b64ddb66be7
frahman5 <faiyam@faiyamrahman.com>
2020-02-21 17:24:09 -0500

    adds getAuth0Users function

---
+++ src/auth/getAuth0Users.ts
@@ -0,0 +1,33 @@
+import request from "request"
+
+export interface Auth0User {
+  email: string
+  family_name: string
+  given_name: string
+  user_id: string
+}
+export const getAuth0Users = async (
+  managementAPIToken: string
+): Promise<Auth0User[]> =>
+  new Promise((resolve, reject) => {
+    request(
+      {
+        method: "Get",
+        url: `https://${process.env.AUTH0_DOMAIN}/api/v2/users`,
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${managementAPIToken}`,
+        },
+        json: true,
+      },
+      (error, response, body) => {
+        if (error) {
+          return reject(error)
+        }
+        if (response.statusCode !== 200) {
+          return reject("Invalid status code <" + response.statusCode + ">")
+        }
+        return resolve(body)
+      }
+    )
+  })
