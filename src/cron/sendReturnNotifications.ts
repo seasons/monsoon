c980d036abe5f0b7ae05791f5fae39465d610172
frahman5 <faiyam@faiyamrahman.com>
2020-02-21 14:59:51 -0500

    updates logic to fix bugs

--- src/cron/sendReturnNotifications.ts
+++ src/cron/sendReturnNotifications.ts
@@ -1,46 +1,59 @@
-import { prisma } from "../prisma"
-import { DateTime } from "luxon"
+import { prisma, Reservation } from "../prisma"
+import { DateTime, Interval } from "luxon"
 import { sendTransactionalEmail } from "../sendTransactionalEmail"
 import { emails } from "../emails"
+import { formatReservationReturnDate } from "../resolvers/Product/formatReservationReturnDate"
 
 export const sendReturnNotifications = async () => {
   const reservations = await prisma.reservations({
     orderBy: "createdAt_DESC",
   })
 
-  const timeDifference = 2
-
   for (const reservation of reservations) {
-    const reservationCreatedAt = DateTime.fromISO(reservation.createdAt)
-    const now = DateTime.local()
-    // Remind customer to return items
-    if (reservationCreatedAt.plus({ days: 28 }) >= now) {
+    if (await returnNoticeNeeded(reservation)) {
+      //   Remind customer to return items
       const user = await prisma
         .reservation({
           id: reservation.id,
         })
         .customer()
         .user()
-
-      const customer = await prisma
-        .reservation({
-          id: reservation.id,
-        })
-        .customer()
-
-      if (customer.plan === "Essential" && reservation) {
-        console.log(reservation)
-
-        sendTransactionalEmail({
-          to: user.email,
-          data: emails.returnReminderData({
-            name: user.firstName,
-            returnDate: "",
-          }),
-        })
-      }
+      sendTransactionalEmail({
+        to: user.email,
+        data: emails.returnReminderData({
+          name: user.firstName,
+          returnDate: formatReservationReturnDate(
+            new Date(reservation.createdAt)
+          ),
+        }),
+      })
+      await prisma.updateReservation({
+        where: { id: reservation.id },
+        data: { reminderSentAt: DateTime.local().toString() },
+      })
     }
   }
+}
+
+const returnNoticeNeeded = async (reservation: Reservation) => {
+  const now = DateTime.local()
+  const reservationCreatedAt = DateTime.fromISO(reservation.createdAt)
+  const twentyEightToThirtyDaysAgo = Interval.fromDateTimes(
+    now.minus({ days: 29 }),
+    now.minus({ days: 28 })
+  )
+  const customer = await prisma
+    .reservation({
+      id: reservation.id,
+    })
+    .customer()
+
+  return (
+    twentyEightToThirtyDaysAgo.contains(reservationCreatedAt) &&
+    !reservation.reminderSentAt &&
+    customer.plan === "Essential" &&
+    !["Cancelled", "Completed"].includes(reservation.status)
+  )
 }
 
 sendReturnNotifications()
