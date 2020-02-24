2f27aa820397a800fd96d95ed165f584da744ad3
Omar Rasheed <oyr2@cornell.edu>
2020-02-24 01:17:50 -0500

    Add more resolvers

--- src/modules/Homepage/homepageResult.resolver.ts
+++ src/modules/Homepage/homepageResult.resolver.ts
@@ -4,6 +4,13 @@ import { Resolver, ResolveProperty } from "@nestjs/graphql"
 export class HomepageResultResolver {
   @ResolveProperty()
   __resolveType(obj) {
-    return obj.__typename
+    if (obj.brand || obj.colorway) {
+      return "Product"
+    } else if (obj.subTitle) {
+      return "Collection"
+    } else if (obj.name) {
+      return "HomepageProductRail"
+    }
+    return null
   }
 }
--- src/modules/Homepage/services/homepage.service.ts
+++ src/modules/Homepage/services/homepage.service.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD:src/modules/Homepage/services/homepage.service.ts
 import { Injectable } from "@nestjs/common"
 import { DBService } from "../../../prisma/DB.service"
-=======
-import { Resolver, ResolveProperty, Query, Context } from "@nestjs/graphql"
-import { DBService } from "../../prisma/DB.service"
->>>>>>> Add login mutation:src/modules/Homepage/homepage.resolver.ts
 
 export enum SectionTitle {
   FeaturedCollection = "Featured collection",
@@ -12,27 +7,12 @@ export enum SectionTitle {
   RecentlyViewed = "Recently viewed",
 }
 
-<<<<<<< HEAD:src/modules/Homepage/services/homepage.service.ts
 @Injectable()
 export class HomepageService {
   constructor(private readonly db: DBService) {}
 
   async getHomepageSections() {
     const productRails = await this.db.query.homepageProductRails(
-=======
-@Resolver("Homepage")
-export class HomepageResolver {
-  constructor(private readonly dbService: DBService) {}
-
-  @Query()
-  async homepage() {
-    return {}
-  }
-
-  @ResolveProperty()
-  async sections() {
-    const productRails = await this.dbService.query.homepageProductRails(
->>>>>>> Add login mutation:src/modules/Homepage/homepage.resolver.ts
       {},
       `{
         name
@@ -53,11 +33,7 @@ export class HomepageResolver {
       {
         type: "Products",
         __typename: "HomepageSection",
-<<<<<<< HEAD:src/modules/Homepage/services/homepage.service.ts
         title: SectionTitle.RecentlyViewed
-=======
-        title: SectionTitle[SectionTitle.RecentlyViewed]
->>>>>>> Add login mutation:src/modules/Homepage/homepage.resolver.ts
       },
     ]
 
