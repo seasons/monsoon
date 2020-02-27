c8b77629d1085c48edfcc04df0f0c87d287dc1ad
frahman5 <faiyam@faiyamrahman.com>
2020-02-27 17:40:19 -0500

    make healthcheck robust to errors

--- src/cron/reportAirtablePrismaSyncHealth.ts
+++ src/cron/reportAirtablePrismaSyncHealth.ts
@@ -5,193 +5,203 @@ import { checkProductsAlignment } from "./airtableToPrismaHealthCheck"
 export const run = async () => {
   const web = new WebClient(process.env.SLACK_CANARY_API_TOKEN)
 
-  // Run report and gather results in a string
-  const [
-    productsInPrismaButNotAirtable,
-    productsInAirtableButNotPrisma,
-    physicalProductsInPrismaButNotAirtable,
-    physicalProductsInAirtableButNotPrisma,
-    productVariantsInPrismaButNotAirtable,
-    productVariantsInAirtableButNotPrisma,
-    productVariantSKUMismatches,
-    prismaSUIDToSKUMismatches,
-    airtableSUIDToSKUMismatches,
-    countMisalignments,
-    prismaTotalPhysicalProductMisalignment,
-    airtableTotalPhysicalProductMisalignment,
-    mismatchingStatuses,
-    reservationsInPrismaButNotAirtable,
-    reservationsInAirtableButNotPrisma,
-    misalignedSUIDsOnReservations,
-    misalignedStatusOnReservations,
-    reservationsWithMoreThanThreeProducts,
-    errors,
-  ] = await checkProductsAlignment()
-
-  // Report it to slack
-  await web.chat.postMessage({
-    channel: process.env.SLACK_DEV_CHANNEL_ID,
-    text: "",
-    blocks: [
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: `:newspaper: *Airtable-Prisma Products Parity Health Check* :newspaper:`,
-        },
-      },
-      {
-        type: "context",
-        elements: [
-          {
+  let message = { channel: process.env.SLACK_DEV_CHANNEL_ID, text: "'" }
+  try {
+    const [
+      productsInPrismaButNotAirtable,
+      productsInAirtableButNotPrisma,
+      physicalProductsInPrismaButNotAirtable,
+      physicalProductsInAirtableButNotPrisma,
+      productVariantsInPrismaButNotAirtable,
+      productVariantsInAirtableButNotPrisma,
+      productVariantSKUMismatches,
+      prismaSUIDToSKUMismatches,
+      airtableSUIDToSKUMismatches,
+      countMisalignments,
+      prismaTotalPhysicalProductMisalignment,
+      airtableTotalPhysicalProductMisalignment,
+      mismatchingStatuses,
+      reservationsInPrismaButNotAirtable,
+      reservationsInAirtableButNotPrisma,
+      misalignedSUIDsOnReservations,
+      misalignedStatusOnReservations,
+      reservationsWithMoreThanThreeProducts,
+      errors,
+    ] = await checkProductsAlignment()
+    throw new Error("yo")
+    message = {
+      ...message,
+      text: "",
+      blocks: [
+        {
+          type: "section",
+          text: {
             type: "mrkdwn",
-            text: `*${new Date().toLocaleDateString()}*`,
+            text: `:newspaper: *Airtable-Prisma Products Parity Health Check* :newspaper:`,
           },
-        ],
-      },
-      ...createReportSection({
-        title:
-          "Do products, physical products, and product variants align in number?",
-        datapoints: [
-          {
-            name: "Products on Prisma but not Airtable",
-            number: productsInPrismaButNotAirtable.length,
-            shouldFlagNum: true,
-          },
-          {
-            name: "Products on Airtable but not Prisma",
-            number: productsInAirtableButNotPrisma.length,
-          },
-          {
-            name: "Physical Products on Prisma but not Airtable",
-            number: physicalProductsInPrismaButNotAirtable.length,
-            shouldFlagNum: true,
-          },
-          {
-            name: "Physical Products on Airtable but not Prisma",
-            number: physicalProductsInAirtableButNotPrisma.length,
-          },
-          {
-            name: "Product Variants on Prisma but not Airtable",
-            number: productVariantsInPrismaButNotAirtable.length,
-            shouldFlagNum: true,
-          },
-          {
-            name: "Product Variants on Airtable but not Prisma",
-            number: productVariantsInAirtableButNotPrisma.length,
-          },
-        ],
-      }),
-      ...createReportSection({
-        title: "Do product variant SKUs match?",
-        datapoints: [
-          {
-            name: "Mismatched product variant SKUs",
-            number: productVariantSKUMismatches.length,
-            shouldFlagNum: true,
-          },
-        ],
-      }),
-      ...createReportSection({
-        title: "Are SUIDs correct?",
-        datapoints: [
-          {
-            name: "Mismatched SUID/SKU combos on Prisma",
-            number: prismaSUIDToSKUMismatches.length,
-            shouldFlagNum: true,
-          },
-          {
-            name: "Mismatched SUID/SKU combos on Airtable",
-            number: airtableSUIDToSKUMismatches.length,
-            shouldFlagNum: true,
-          },
-        ],
-      }),
-      ...createReportSection({
-        title: "Are the counts aligned?",
-        datapoints: [
-          {
-            name:
-              "Prisma: Number of Product Variants with incorrect number of physical products attached",
-            number: prismaTotalPhysicalProductMisalignment.length,
-            shouldFlagNum: true,
-          },
-          {
-            name:
-              "Airtable: Number of Product Variants with incorrect number of physical products attached",
-            number: airtableTotalPhysicalProductMisalignment.length,
-            shouldFlagNum: true,
-          },
-          {
-            name: "Mismatched counts",
-            number: countMisalignments.length,
-            shouldFlagNum: true,
-          },
-        ],
-      }),
-      ...createReportSection({
-        title: "Are physical product inventory statuses aligned?",
-        datapoints: [
-          {
-            name:
-              "Number of physical products with mismatching inventory statuses",
-            number: mismatchingStatuses.length,
-            shouldFlagNum: true,
-          },
-        ],
-      }),
-      ...createReportSection({
-        title: "Are reservations aligned?",
-        datapoints: [
-          {
-            name: "Reservations in prisma but not airtable",
-            number: reservationsInPrismaButNotAirtable.length,
-            shouldFlagNum: true,
-          },
-          {
-            name: "Reservations in airtable but not prisma",
-            number: reservationsInAirtableButNotPrisma.length,
-            shouldFlagNum: true,
-          },
-          {
-            name: "Reservations with mismatching products",
-            number: misalignedSUIDsOnReservations.length,
-            shouldFlagNum: true,
-          },
-          {
-            name: "Reservations with mismatching statuses",
-            number: misalignedStatusOnReservations.length,
-            shouldFlagNum: true,
-          },
-          {
-            name: "Reservations with more than 3 products",
-            number: reservationsWithMoreThanThreeProducts.length,
-            shouldFlagNum: true,
-          },
-        ],
-      }),
-      ...createReportSection({
-        title: "Other",
-        datapoints: [
-          {
-            name: "Errors",
-            number: errors.length,
-          },
-        ],
-        divider: false,
-      }),
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text:
-            `Note: \`Highlighted numbers\` need attention. For more details` +
-            ` work with the health check script locally using \`yarn healthCheck:production\`` +
-            `, printing more details as needed`,
         },
-      },
-    ],
-  })
+        {
+          type: "context",
+          elements: [
+            {
+              type: "mrkdwn",
+              text: `*${new Date().toLocaleDateString()}*`,
+            },
+          ],
+        },
+        ...createReportSection({
+          title:
+            "Do products, physical products, and product variants align in number?",
+          datapoints: [
+            {
+              name: "Products on Prisma but not Airtable",
+              number: productsInPrismaButNotAirtable.length,
+              shouldFlagNum: true,
+            },
+            {
+              name: "Products on Airtable but not Prisma",
+              number: productsInAirtableButNotPrisma.length,
+            },
+            {
+              name: "Physical Products on Prisma but not Airtable",
+              number: physicalProductsInPrismaButNotAirtable.length,
+              shouldFlagNum: true,
+            },
+            {
+              name: "Physical Products on Airtable but not Prisma",
+              number: physicalProductsInAirtableButNotPrisma.length,
+            },
+            {
+              name: "Product Variants on Prisma but not Airtable",
+              number: productVariantsInPrismaButNotAirtable.length,
+              shouldFlagNum: true,
+            },
+            {
+              name: "Product Variants on Airtable but not Prisma",
+              number: productVariantsInAirtableButNotPrisma.length,
+            },
+          ],
+        }),
+        ...createReportSection({
+          title: "Do product variant SKUs match?",
+          datapoints: [
+            {
+              name: "Mismatched product variant SKUs",
+              number: productVariantSKUMismatches.length,
+              shouldFlagNum: true,
+            },
+          ],
+        }),
+        ...createReportSection({
+          title: "Are SUIDs correct?",
+          datapoints: [
+            {
+              name: "Mismatched SUID/SKU combos on Prisma",
+              number: prismaSUIDToSKUMismatches.length,
+              shouldFlagNum: true,
+            },
+            {
+              name: "Mismatched SUID/SKU combos on Airtable",
+              number: airtableSUIDToSKUMismatches.length,
+              shouldFlagNum: true,
+            },
+          ],
+        }),
+        ...createReportSection({
+          title: "Are the counts aligned?",
+          datapoints: [
+            {
+              name:
+                "Prisma: Number of Product Variants with incorrect number of physical products attached",
+              number: prismaTotalPhysicalProductMisalignment.length,
+              shouldFlagNum: true,
+            },
+            {
+              name:
+                "Airtable: Number of Product Variants with incorrect number of physical products attached",
+              number: airtableTotalPhysicalProductMisalignment.length,
+              shouldFlagNum: true,
+            },
+            {
+              name: "Mismatched counts",
+              number: countMisalignments.length,
+              shouldFlagNum: true,
+            },
+          ],
+        }),
+        ...createReportSection({
+          title: "Are physical product inventory statuses aligned?",
+          datapoints: [
+            {
+              name:
+                "Number of physical products with mismatching inventory statuses",
+              number: mismatchingStatuses.length,
+              shouldFlagNum: true,
+            },
+          ],
+        }),
+        ...createReportSection({
+          title: "Are reservations aligned?",
+          datapoints: [
+            {
+              name: "Reservations in prisma but not airtable",
+              number: reservationsInPrismaButNotAirtable.length,
+              shouldFlagNum: true,
+            },
+            {
+              name: "Reservations in airtable but not prisma",
+              number: reservationsInAirtableButNotPrisma.length,
+              shouldFlagNum: true,
+            },
+            {
+              name: "Reservations with mismatching products",
+              number: misalignedSUIDsOnReservations.length,
+              shouldFlagNum: true,
+            },
+            {
+              name: "Reservations with mismatching statuses",
+              number: misalignedStatusOnReservations.length,
+              shouldFlagNum: true,
+            },
+            {
+              name: "Reservations with more than 3 products",
+              number: reservationsWithMoreThanThreeProducts.length,
+              shouldFlagNum: true,
+            },
+          ],
+        }),
+        ...createReportSection({
+          title: "Other",
+          datapoints: [
+            {
+              name: "Errors",
+              number: errors.length,
+            },
+          ],
+          divider: false,
+        }),
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text:
+              `Note: \`Highlighted numbers\` need attention. For more details` +
+              ` work with the health check script locally using \`yarn healthCheck:production\`` +
+              `, printing more details as needed`,
+          },
+        },
+      ],
+    } as any
+  } catch (err) {
+    message = {
+      ...message,
+      text:
+        `Error while running airtable/prisma health check. Please debug it ` +
+        `locally using \`yarn healthCheck:production\`\n${err}`,
+    } as any
+  }
+
+  await web.chat.postMessage(message)
 }
 
 interface DataPoint {
