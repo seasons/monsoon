ef4c5caa1c9d6e779abbf7833f3226d8f3922cfe
Omar Rasheed <oyr2@cornell.edu>
2020-02-29 18:07:04 -0500

    Add test to me query

---
+++ src/modules/User/me.resolvers.spec.ts
@@ -0,0 +1,25 @@
+import { graphqlTestCall } from '../../tests/utils/gqlTestClient';
+
+describe('MeResolver', () => {
+
+  describe("Me", () => {
+    it("allows logged in user fetch their information", async () => {
+      const response = await graphqlTestCall(
+        `
+          {
+            me {
+              user {
+                id
+                firstName
+                lastName
+                email
+              }
+            }
+          }
+        `
+      )
+
+      expect(response.data.me.user.id).toEqual("ck2ge3byx06c70757us4chh91")
+    })
+  })
+});
