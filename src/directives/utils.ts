199dc6085fa41770ae3098505adf43231a0a6685
Omar Rasheed <oyr2@cornell.edu>
2020-03-02 01:33:14 -0500

    Add isOwner directive

--- src/directives/utils.ts
+++ src/directives/utils.ts
@@ -1,5 +1,19 @@
+import { get } from "lodash"
+import { DBService } from "../prisma/db.service"
+
+const db = new DBService()
+
 export class AuthError extends Error {
   constructor() {
     super("Not authorized")
   }
+}
+
+export const isRequestingUserAlsoOwner = (userId, type, typeId) =>
+  db.exists[type]({ id: typeId, user: { id: userId } })
+
+export const getUserIfExists = ctx => {
+  const user = get(ctx, "req.user")
+  if (!user) throw new AuthError()
+  return user
 }
