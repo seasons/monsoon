2208aa613b839b6cb1a93530a19112935c9bd166
Omar Rasheed <oyr2@cornell.edu>
2020-03-04 15:53:52 -0500

    Add isOwnerOrHasRole directive

--- src/directives/HasRole.ts
+++ src/directives/HasRole.ts
@@ -1,12 +1,8 @@
-import { get } from "lodash"
-import { AuthError } from "./utils"
+import { getEnforcedUser } from "./utils"
 
 export function hasRole(next, source, { roles }, ctx) {
   // Todo: test when addCustomerDetails mutation is complete
-
-  // Extract the auth0Id from the user object on the request
-  const user = get(ctx, "req.user")
-  if (!user) throw new AuthError()
+  const user = getEnforcedUser(ctx)
 
   if (!roles.includes(user.role)) {
     throw new Error(`Unauthorized, incorrect role`)
