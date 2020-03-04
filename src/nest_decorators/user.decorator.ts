bba0356982c0a1095ea196dae4923c3fcb5f4a21
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-03-04 17:26:19 -0500

    Specify type with Prisma user type on User decorator

--- src/nest_decorators/user.decorator.ts
+++ src/nest_decorators/user.decorator.ts
@@ -1,15 +1,9 @@
 import { createParamDecorator } from "@nestjs/common"
+import { User as PrismaUser } from "../prisma"
 
-export const User = createParamDecorator((data, [root, args, ctx, info]) => {
-  if (!ctx.req.user) return null
-  return {
-    id: ctx.req.user.id,
-    email: ctx.req.user.email,
-    lastName: ctx.req.user.lastName,
-    firstName: ctx.req.user.firstName,
-    auth0Id: ctx.req.user.auth0Id,
-    createdAt: ctx.req.user.createdAt,
-    role: ctx.req.user.role,
-    updatedAt: ctx.req.user.updatedAt,
+export const User = createParamDecorator(
+  (data, [root, args, ctx, info]): PrismaUser => {
+    if (!ctx.req.user) return null
+    return ctx.req.user
   }
-})
+)
