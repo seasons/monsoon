99f3f229701f64d5c1bb1c0e992a22ec3b8a2f9b
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-03-20 16:20:26 -0400

    remove extraneous logger option

--- src/index.ts
+++ src/index.ts
@@ -38,9 +38,7 @@ server.use(
 )
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server), {
-    logger: ['debug', 'error', 'log', 'warn', 'verbose']
-  })
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server))
   app
   await app.listen(process.env.PORT ? process.env.PORT : 4000, () =>
     console.log(`🚀 Server ready at ${process.env.PORT || 4000}`)
