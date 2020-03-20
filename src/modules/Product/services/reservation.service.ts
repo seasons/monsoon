c80a23c127b6bfba19fac873c0bead255d9185fe
Omar Rasheed <oyr2@cornell.edu>
2020-03-20 02:14:49 -0700

    Add reservations cron service

--- src/modules/Product/services/reservation.service.ts
+++ src/modules/Product/services/reservation.service.ts
@@ -168,6 +168,18 @@ export class ReservationService {
     return reservationReturnData
   }
 
+  formatReservationReturnDate(reservationCreatedAtDate: Date) {
+    const returnDate = new Date(reservationCreatedAtDate)
+    returnDate.setDate(reservationCreatedAtDate.getDate() + 30)
+    return returnDate.toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    })
+  }
+  
+
   private async getLatestReservation(
     customer: Customer
   ): Promise<ReservationWithProductVariantData | null> {
