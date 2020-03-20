b8a2652ca9189ba9d453cf133eafe9dcc552173a
Omar Rasheed <oyr2@cornell.edu>
2020-03-20 02:14:49 -0700

    Add reservations cron service

--- src/modules/Cron/cron.module.ts
+++ src/modules/Cron/cron.module.ts
@@ -2,6 +2,7 @@ import { Module, forwardRef } from '@nestjs/common';
 import { UsersScheduledJobs } from './services/users.service';
 import { AirtableModule, EmailModule, UserModule } from '..';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { ReservationsService } from './services/reservations.service';
 
 @Module({
   imports: [
@@ -10,6 +11,6 @@ import { PrismaModule } from '../../prisma/prisma.module';
     forwardRef(() => PrismaModule),
     forwardRef(() => UserModule)
   ],
-  providers: [UsersScheduledJobs],
+  providers: [ReservationsService, UsersScheduledJobs],
 })
 export class CronModule {}
