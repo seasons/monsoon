13fddcfc97631618a86072ad6358e0158cf9ebd9
Kevin Chan <kevinchan159@gmail.com>
2020-03-24 16:55:09 -0400

    Adds mock data code

--- src/modules/FAQ/queries/faq.queries.ts
+++ src/modules/FAQ/queries/faq.queries.ts
@@ -1,14 +1,15 @@
 import { Resolver, Query } from "@nestjs/graphql"
 import { FAQService } from "../services/faq.service"
+import { User, Customer } from "../../../nest_decorators"
 
 @Resolver()
 export class FAQQueriesResolver {
-  constructor(private readonly faqService: FAQService) {}
+  constructor(private readonly faqService: FAQService) { }
 
   @Query()
-  faq() {
+  faq(@User() user) {
     return {
-      sections: this.faqService.getSections()
+      sections: this.faqService.getSections(user)
     }
   }
 
--- src/modules/FAQ/services/faq.service.ts
+++ src/modules/FAQ/services/faq.service.ts
@@ -1,10 +1,68 @@
 import { Injectable } from '@nestjs/common'
+import { DBService } from "../../../prisma/db.service"
+import { RecentlyViewedProduct, BagItem } from "../../../prisma"
+import { head } from "lodash"
+import { PrismaClientService } from "../../../prisma/client.service"
 
 
 @Injectable()
 export class FAQService {
+  constructor(
+    private readonly db: DBService,
+    private readonly prisma: PrismaClientService,
+  ) { }
 
-  getSections() {
+  async getSections(user) {
+    // TODO: Remove before merging. Currently using to create mock data
+    // const items = [
+    //   "ck7z06yy80ne60724ex76we5n",
+    //   "ck7z027rb0eka072417wdwsjn",
+    //   "ck7z06h5w0mfx0724bektpq65",
+    // ]
+    // const variants = await Promise.all(
+    //   items.map(async item => {
+    //     // const variant = this.prisma.client.productVariant({ id: item })
+    //     return {
+    //       id: await this.prisma.client.productVariant({ id: item }).id(),
+    //       name: await this.prisma.client.productVariant({ id: item }).product().name(),
+    //       retailPrice: await this.prisma.client.productVariant({ id: item }).product().retailPrice(),
+    //     }
+    //   })
+    // )
+    // console.log("VARIANTS:", variants)
+    // const reservationFeedback = await this.prisma.client.createReservationFeedback({
+    //   feedbacks: {
+    //     create: variants.map(variant => ({
+    //       isCompleted: false,
+    //       questions: {
+    //         create: [
+    //           {
+    //             question: `How many times did you wear this ${variant.name}?`,
+    //             options: { set: ["More than 6 times", "3-5 times", "1-2 times", "0 times"] },
+    //             type: "MultipleChoice",
+    //           },
+    //           {
+    //             question: `Would you buy it at retail for $${variant.retailPrice}?`,
+    //             options: { set: ["Would buy at a discount", "Buy below retail", "Buy at retail", "Would only rent"] },
+    //             type: "MultipleChoice",
+    //           },
+    //           {
+    //             question: `Did it fit as expected?`,
+    //             options: { set: ["Fit too big", "Fit true to size", "Ran small", "Didn’t fit at all"] },
+    //             type: "MultipleChoice",
+    //           },
+    //         ]
+    //       },
+    //       variant: { connect: { id: variant.id } }
+    //     })),
+    //   },
+    //   user: {
+    //     connect: {
+    //       id: user.id
+    //     }
+    //   },
+    // })
+    // console.log("MADE FEEDBACK:", reservationFeedback)
     return [
       {
         title: "Need to return your items?",
