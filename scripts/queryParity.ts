224064c79ec5b2c96ff6387338e2fb10d80a111e
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-03-24 16:33:19 -0400

    add test queries for product and products

--- scripts/queryParity.ts
+++ scripts/queryParity.ts
@@ -1,6 +1,50 @@
 import { request } from "graphql-request"
 import expect from "expect"
 
+const productFields = `
+id
+slug
+name
+brand {
+  id
+}
+category {
+  id
+}
+type
+description
+externalURL
+images
+modelHeight
+modelSize {
+  id
+  display
+}
+retailPrice
+color {
+  id
+}
+secondaryColor {
+  id
+}
+tags
+functions {
+  id
+}
+availableSizes {
+  id
+  slug
+}
+innerMaterials
+outerMaterials
+variants {
+  id
+}
+status
+isSaved
+createdAt
+updatedAt`
+
 const queries = {
   brand: `
   {
@@ -35,6 +79,20 @@ const queries = {
       }
     }
   }`,
+  product: `
+  {
+    product(where: {id: "ck7yztt6j01mh07249657q5mm"}) {
+      ${productFields}      
+    }
+  }
+  `,
+  products: `
+  {
+    products(orderBy: id_ASC) {
+      ${productFields}
+    }
+  }
+  `,
 }
 const oURL = "https://monsoon-staging.herokuapp.com"
 const nURL = "https://monsoon-nest-staging.herokuapp.com"
