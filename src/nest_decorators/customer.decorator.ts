bbfcdc9ccfa4a572b634e457a52c4c04ee119b5c
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-03-25 17:40:19 -0400

    optimize customer decorator

--- src/nest_decorators/customer.decorator.ts
+++ src/nest_decorators/customer.decorator.ts
@@ -6,10 +6,17 @@ const prisma = new PrismaService()
 
 export const Customer = createParamDecorator(
   async (data, [root, args, ctx, info]): Promise<PrismaCustomer> => {
+    if (!!ctx.customer || ctx.customer === null) {
+      return ctx.customer
+    }
+
     const { id: userId } = ctx.req.user ? ctx.req.user : { id: "" }
     const customerArray = await prisma.client.customers({
       where: { user: { id: userId } },
     })
-    return customerArray.length > 0 ? customerArray[0] : null
+    const customer = customerArray.length > 0 ? customerArray[0] : null
+    ctx.customer = customer
+
+    return customer
   }
 )
