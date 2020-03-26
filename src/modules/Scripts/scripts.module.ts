f0c60f62e1fc6764e89ae285862916ee112ca8a9
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-03-26 13:51:23 -0400

    adds syncPrismaPrisma command to cli

--- src/modules/Scripts/scripts.module.ts
+++ src/modules/Scripts/scripts.module.ts
@@ -1,19 +1,13 @@
 import { Module } from "@nestjs/common"
-import { CommandModule } from "nestjs-command";
-import { UserModule } from "../User/user.module";
-import { PrismaModule } from "../../prisma/prisma.module";
-import { ScriptsService } from "./services/scripts.service";
-import { UserCommands } from "./commands/user.command";
+import { CommandModule } from "nestjs-command"
+import { UserModule } from "../User/user.module"
+import { PrismaModule } from "../../prisma/prisma.module"
+import { ScriptsService } from "./services/scripts.service"
+import { UserCommands } from "./commands/user.command"
+import { SyncCommands } from "./commands/sync.command"
 
 @Module({
-  imports: [
-    CommandModule,
-    PrismaModule,
-    UserModule
-  ],
-  providers: [
-    ScriptsService,
-    UserCommands
-  ],
+  imports: [CommandModule, PrismaModule, UserModule],
+  providers: [ScriptsService, UserCommands, SyncCommands],
 })
 export class ScriptsModule {}
