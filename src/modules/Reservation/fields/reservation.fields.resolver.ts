35e8365889a690fdde27570c843b2a3eb836f2db
Kieran Gillen <kgillen@gmail.com>
2020-03-31 15:47:11 -0400

    Fixes customer fields

--- src/modules/Reservation/fields/reservation.fields.resolver.ts
+++ src/modules/Reservation/fields/reservation.fields.resolver.ts
@@ -1,23 +1,9 @@
-import { Parent, ResolveField, Resolver } from "@nestjs/graphql"
-import { PrismaService } from "../../../prisma/prisma.service"
+import { ResolveField, Resolver } from "@nestjs/graphql"
 import { ReservationUtilsService } from "../services/reservationUtils.service"
 
 @Resolver("Reservation")
 export class ReservationFieldsResolver {
-  constructor(
-    private readonly prisma: PrismaService,
-    private readonly reservationService: ReservationUtilsService
-  ) {}
-
-  @ResolveField()
-  async products(@Parent() parent) {
-    const reservationProducts = this.prisma.client
-      .reservation({
-        id: parent.id,
-      })
-      .products()
-    return reservationProducts
-  }
+  constructor(private readonly reservationService: ReservationUtilsService) {}
 
   @ResolveField()
   returnDateDisplay() {
