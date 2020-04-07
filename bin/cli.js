b841eb16c49527a2b9bd7725e9b1e6cbb969d289
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-04-07 11:38:26 -0400

    fixes aliasing error

---
+++ bin/cli.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+// require("module-alias/register")
+require("../dist/cli")
--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,12 @@
-#!/usr/bin/env ts-node
+import "module-alias/register"
+
+import * as Airtable from "airtable"
+
+import { CommandModule, CommandService } from "nestjs-command"
 
 import { NestFactory } from "@nestjs/core"
-import { CommandModule, CommandService } from "nestjs-command"
-import * as Airtable from "airtable"
+// Must be imported after dotenv.configure() to ensure that env vars are being loaded before making module
+import { ScriptsModule } from "./modules/Scripts/scripts.module"
 import dotenv from "dotenv"
 
 dotenv.config()
@@ -11,9 +15,6 @@ Airtable.configure({
   endpointUrl: "https://api.airtable.com",
   apiKey: process.env.AIRTABLE_KEY,
 })
-
-// Must be imported after dotenv.configure() to ensure that env vars are being loaded before making module
-import { ScriptsModule } from "../src/modules/Scripts/scripts.module"
 
 async function setup() {
   const app = await NestFactory.createApplicationContext(ScriptsModule)
