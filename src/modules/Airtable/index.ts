26933a2eba77f9063a4d47edd4d2da362d9623d5
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-04-10 10:31:09 -0400

    removes type exports from airtable/index.ts

--- src/modules/Airtable/index.ts
+++ src/modules/Airtable/index.ts
@@ -2,9 +2,3 @@ export { AirtableService } from "./services/airtable.service"
 export { AirtableBaseService } from "./services/airtable.base.service"
 export { AirtableUtilsService } from "./services/airtable.utils.service"
 export { AirtableModule } from "./airtable.module"
-// export {
-//   AirtableData,
-//   AirtableInventoryStatus,
-//   AirtableModelName,
-//   AirtableProductVariantCounts,
-// } from "./airtable.types"
