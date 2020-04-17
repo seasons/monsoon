d91a64d1802d7ac7e520075dbad6d2df9c91518d
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-04-17 15:16:15 -0400

    adds updateProductSlugs and productNameDuplicates scripts

---
+++ src/scripts/updateProductSlugs.ts
@@ -0,0 +1,68 @@
+import "module-alias/register"
+
+import * as Airtable from "airtable"
+
+import { AirtableBaseService } from "../modules/Airtable/services/airtable.base.service"
+import { AirtableService } from "../modules/Airtable/services/airtable.service"
+import { AirtableUtilsService } from "../modules/Airtable/services/airtable.utils.service"
+import { PrismaService } from "../prisma/prisma.service"
+import slugify from "slugify"
+
+Airtable.configure({
+  endpointUrl: "https://api.airtable.com",
+  apiKey: process.env.AIRTABLE_KEY,
+})
+
+const updateSlugs = async () => {
+  const ps = new PrismaService()
+  const abs = new AirtableBaseService()
+  const as = new AirtableService(abs, new AirtableUtilsService(abs))
+
+  /*
+  Get all products
+  For each product:
+    -> generate the brandCode-name-color slug in prisma
+    -> set the slug in prisma
+    -> set the slug in airtable
+  */
+  const allPrismaProducts = await ps.binding.query.products(
+    {},
+    `
+  {
+      slug
+      brand {
+        brandCode
+      }
+      color {
+        name
+        colorCode
+      }
+  }`
+  )
+  const allAirtableProducts = await as.getAllProducts()
+  for (const prismaProd of allPrismaProducts) {
+    if (prismaProd.slug.includes(prismaProd.brand.brandCode.toLowerCase())) {
+      continue
+    }
+    const correspondingAirtableProduct = allAirtableProducts.find(
+      a => a.model.slug === prismaProd.slug
+    )
+    const newSlug = slugify(
+      prismaProd.brand.brandCode + " " + prismaProd.slug
+    ).toLowerCase()
+    await ps.client.updateProduct({
+      where: { slug: prismaProd.slug },
+      data: { slug: newSlug },
+    })
+    try {
+      await correspondingAirtableProduct.patchUpdate({
+        Slug: newSlug,
+      })
+    } catch (err) {
+      console.log(newSlug)
+      console.log(err)
+    }
+  }
+}
+
+updateSlugs()
