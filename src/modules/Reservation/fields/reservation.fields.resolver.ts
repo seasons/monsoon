59f34268d3c63e183e69a0c63950830f6b481469
Luc Succes <l2succes@gmail.com>
2020-04-22 17:56:48 -0400

    Adds reservation.images

--- src/modules/Reservation/fields/reservation.fields.resolver.ts
+++ src/modules/Reservation/fields/reservation.fields.resolver.ts
@@ -1,7 +1,8 @@
-import { Info, Parent, ResolveField, Resolver } from "@nestjs/graphql"
+import { Parent, ResolveField, Resolver } from "@nestjs/graphql"
 
 import { PrismaService } from "@prisma/prisma.service"
 import { ReservationUtilsService } from "../services/reservation.utils.service"
+import head from "lodash"
 
 @Resolver("Reservation")
 export class ReservationFieldsResolver {
@@ -18,5 +19,42 @@ export class ReservationFieldsResolver {
     return this.reservationService.formatReservationReturnDate(
       new Date(reservation?.createdAt)
     )
+  }
+
+  @ResolveField()
+  async returnAt(@Parent() parent) {
+    const reservation = await this.prisma.client.reservation({
+      id: parent.id,
+    })
+    return new Date(reservation?.createdAt).toUTCString()
+  }
+
+  @ResolveField()
+  async images(@Parent() parent) {
+    const reservation = await this.prisma.binding.query.reservation(
+      {
+        where: {
+          id: parent.id,
+        },
+      },
+      `
+      {
+        products {
+          id
+          productVariant {
+            product {
+              images
+            }
+          }
+        }
+      }
+      `
+    )
+    const products = reservation.products
+    const firstImages = products.map(product => {
+      return head(product.productVariant.product.images)
+    })
+
+    return firstImages
   }
 }
