3c7479fd75c6329a8714032f4aecf35390a28773
Luc Succes <l2succes@gmail.com>
2020-04-30 15:44:23 -0400

    Adds Image Upload support

--- src/modules/Image/image.module.ts
+++ src/modules/Image/image.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common"
 
+import { Upload } from "./models/Upload.model"
+import { ImageMutationsResolver } from "./mutations/image.mutations.resolver"
 import { ImageResizeService } from "./services/imageResize.service"
 
 @Module({
-  providers: [ImageResizeService],
+  providers: [ImageResizeService, ImageMutationsResolver, Upload],
   exports: [ImageResizeService],
 })
 export class ImageModule {}
---
+++ src/modules/Image/mutations/image.mutations.resolver.ts
@@ -0,0 +1,28 @@
+import { Args, Info, Mutation, Resolver } from "@nestjs/graphql"
+import AWS from "aws-sdk"
+
+@Resolver()
+export class ImageMutationsResolver {
+  private s3 = new AWS.S3()
+
+  @Mutation()
+  async uploadImage(@Args("image") image) {
+    console.log("Hello file", image)
+
+    const file = await image
+    const { createReadStream, filename, mimetype } = file
+    const fileStream = createReadStream()
+
+    // Here stream it to S3
+    // Enter your bucket name here next to "Bucket: "
+    const uploadParams = {
+      Bucket: "seasons-images",
+      Key: filename,
+      Body: fileStream,
+    }
+    const result = await this.s3.upload(uploadParams).promise()
+
+    console.log(result)
+    return "Nice !"
+  }
+}
