8b3641fa3082bfefe7e32325a87e233960b930cc
Kevin Chan <kevinchan159@gmail.com>
2020-04-30 16:40:35 -0400

    Removes s3 from image mutations

--- src/modules/Image/mutations/image.mutations.resolver.ts
+++ src/modules/Image/mutations/image.mutations.resolver.ts
@@ -1,12 +1,9 @@
 import { Args, Info, Mutation, Resolver } from "@nestjs/graphql"
-import AWS from "aws-sdk"
 
 import { ImageService } from "../services/image.service"
 
 @Resolver()
 export class ImageMutationsResolver {
-  private s3 = new AWS.S3()
-
   constructor(private readonly imageService: ImageService) {}
 
   @Mutation()
