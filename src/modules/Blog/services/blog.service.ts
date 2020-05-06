4ef5e4d0a077c8bb6301ca444802cd7c93461d2a
Kieran Gillen <kgillen@gmail.com>
2020-05-06 12:15:06 -0400

    Returns only published posts

--- src/modules/Blog/services/blog.service.ts
+++ src/modules/Blog/services/blog.service.ts
@@ -1,32 +1,38 @@
 import { Injectable } from "@nestjs/common"
 import Webflow from "webflow-api"
 
-const { WEBFLOW_KEY, WEBFLOW_SITE_ID } = process.env
+const { WEBFLOW_KEY, WEBFLOW_COLLECTION_ID } = process.env
 
 @Injectable()
 export class BlogService {
   private webflow = new Webflow({ token: WEBFLOW_KEY })
 
   async getCollections() {
-    const result = await this.webflow.collections({ siteId: WEBFLOW_SITE_ID })
+    const result = await this.webflow.collections({
+      siteId: WEBFLOW_COLLECTION_ID,
+    })
     return result
   }
 
   async getPosts({
     collectionId,
-    skip,
     limit,
   }: {
     collectionId: string
-    skip: number
-    limit: number
+    limit?: number
   }) {
-    const posts = await this.webflow.items({
+    const allPosts = await this.webflow.items({
       collectionId,
-      skip,
-      limit,
     })
 
-    return posts
+    const publishedPosts = allPosts?.items?.filter(post => {
+      return !post._archived && !post._draft
+    })
+
+    if (limit) {
+      return publishedPosts.slice(0, limit)
+    }
+
+    return publishedPosts
   }
 }
