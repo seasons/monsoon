12ac002c5a736a59898a82378721b427116dd8e0
Omar Rasheed <oyr2@cornell.edu>
2020-05-14 04:27:51 -0700

    Add data reset to script

--- src/tests/setupJest.ts
+++ src/tests/setupJest.ts
@@ -3,5 +3,3 @@ import path from "path"
 import dotenv from "dotenv"
 
 dotenv.config({ path: path.resolve(process.cwd(), ".env") })
-
-process.env.PRISMA_ENDPOINT = "http://localhost:4477"
--- src/tests/test.sh
+++ src/tests/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PRISMA_ENDPOINT="http://localhost:4477/monsoon/test";
 container_name=monsoon_prisma_testing_1
 docker_running=$( docker inspect -f {{.State.Running}} $container_name )
 if [ $docker_running == false ]
@@ -6,7 +7,7 @@ then
   echo "Starting up new docker container"
   docker-compose up -d;
   sleep 20;
-  export PRISMA_ENDPOINT="http://localhost:4477";
   yarn prisma deploy;
 fi
+yarn prisma reset -f;
 yarn jest --verbose;
