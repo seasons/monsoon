fc17e5f7cf04e4fbeafa0aa6b016e609a9e30658
Omar Rasheed <oyr2@cornell.edu>
2020-05-26 02:06:06 -0700

    Change to use bash

--- src/tests/setupJest.ts
+++ src/tests/setupJest.ts
@@ -4,3 +4,5 @@ import dotenv from "dotenv"
 
 dotenv.config({ path: path.resolve(process.cwd(), ".env.testing") })
 dotenv.config({ path: path.resolve(process.cwd(), ".env") })
+
+console.log(process.env.PRISMA_ENDPOINT)
