6784ae2bbb1aba13dd1d6eb75a42bf96fcc89801
Omar Rasheed <oyr2@cornell.edu>
2020-05-26 03:30:58 -0700

    Remove print statement in setupJest

--- src/tests/setupJest.ts
+++ src/tests/setupJest.ts
@@ -4,9 +4,3 @@ import dotenv from "dotenv"
 
 dotenv.config({ path: path.resolve(process.cwd(), ".env.testing") })
 dotenv.config({ path: path.resolve(process.cwd(), ".env") })
-
-console.log(
-  process.env.PRISMA_ENDPOINT == "http://localhost:4477/monsoon/test"
-    ? "true"
-    : "false"
-)
