e9396bfc072b7179dce18a72e6e898289b0408e4
Luc Succes <l2succes@gmail.com>
2020-05-26 09:13:20 -0400

    Update cron.module imports

--- src/modules/Cron/cron.module.ts
+++ src/modules/Cron/cron.module.ts
@@ -1,6 +1,7 @@
 import { AirtableModule } from "@modules/Airtable/airtable.module"
 import { EmailModule } from "@modules/Email/email.module"
 import { ErrorModule } from "@modules/Error/error.module"
+import { ReservationModule } from "@modules/Reservation/reservation.module"
 import { ShippingModule } from "@modules/Shipping/shipping.module"
 import { SlackModule } from "@modules/Slack/slack.module"
 import { UserModule } from "@modules/User/user.module"
@@ -8,19 +9,20 @@ import { UtilsModule } from "@modules/Utils/utils.module"
 import { Module } from "@nestjs/common"
 import { PrismaModule } from "@prisma/prisma.module"
 
-import { DataScheduledJobs } from "./services/data.service"
-import { ReservationScheduledJobs } from "./services/reservations.service"
-import { UsersScheduledJobs } from "./services/users.service"
+import { DataScheduledJobs } from "./services/data.job.service"
+import { ReservationScheduledJobs } from "./services/reservations.job.service"
+import { UsersScheduledJobs } from "./services/users.job.service"
 
 @Module({
   imports: [
     AirtableModule,
     EmailModule,
+    ErrorModule,
     PrismaModule,
     ShippingModule,
     SlackModule,
     UserModule,
-    ErrorModule,
+    ReservationModule,
     UtilsModule,
   ],
   providers: [ReservationScheduledJobs, UsersScheduledJobs, DataScheduledJobs],
