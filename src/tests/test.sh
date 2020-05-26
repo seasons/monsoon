39c25d89085cbdbb32b898fdcf4773ea4c9e4517
Omar Rasheed <oyr2@cornell.edu>
2020-05-26 02:52:29 -0700

    Remove silent to debug docker issues

--- src/tests/test.sh
+++ src/tests/test.sh
@@ -18,7 +18,7 @@ docker_running=$( docker inspect -f {{.State.Running}} $container_name )
 if [[ $docker_running == false ]]
 then
   echo "Starting up new docker container"
-  docker-compose up -d;
+  docker-compose up;
   sleep 20;
   yarn prisma deploy -e .env.testing
   sh alter_db.sh -p $POSTGRES_PASSWORD -t $POSTGRES_TABLE -d $POSTGRES_DATABASE &>/dev/null
