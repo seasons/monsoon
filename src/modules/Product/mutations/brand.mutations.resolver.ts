7065e6f7f04634503ed044378d6dda5cb1af6d3f
Kevin Chan <kevinchan159@gmail.com>
2020-06-17 15:04:09 -0400

    Cleanup for PR

--- src/modules/Product/mutations/brand.mutations.resolver.ts
+++ src/modules/Product/mutations/brand.mutations.resolver.ts
@@ -1,9 +1,5 @@
-import { User } from "@app/decorators"
-import { ReservationService } from "@modules/Reservation"
-import { Args, Info, Mutation, Resolver } from "@nestjs/graphql"
+import { Args, Mutation, Resolver } from "@nestjs/graphql"
 import { PrismaService } from "@prisma/prisma.service"
-
-import { ProductVariantService } from "../services/productVariant.service"
 
 @Resolver("Brand")
 export class BrandMutationsResolver {
@@ -11,7 +7,6 @@ export class BrandMutationsResolver {
 
   @Mutation()
   async createBrand(@Args() { input }) {
-    console.log("DATA:", input)
     return await this.prisma.client.createBrand(input)
   }
 }
