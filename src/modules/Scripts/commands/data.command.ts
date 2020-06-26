5fd31fb9c13ed266d1e2310be0d789ffc9379517
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-06-26 14:19:49 -0400

    adds get reservation history command

--- src/modules/Scripts/commands/data.command.ts
+++ src/modules/Scripts/commands/data.command.ts
@@ -1,17 +1,22 @@
 import { DataScheduledJobs } from "@app/modules/Cron/services/data.job.service"
-import { Injectable } from "@nestjs/common"
+import { PrismaService } from "@app/prisma/prisma.service"
+import { Injectable, Logger } from "@nestjs/common"
 import { ModuleRef } from "@nestjs/core"
-import { Command, Option } from "nestjs-command"
+import { pick } from "lodash"
+import { Command, Option, Positional } from "nestjs-command"
 
 import { PrismaEnvOption } from "../scripts.decorators"
 import { ScriptsService } from "../services/scripts.service"
 
 @Injectable()
 export class DataCommands {
+  private readonly logger = new Logger(DataCommands.name)
+
   constructor(
     private readonly scriptsService: ScriptsService,
     private readonly dataJobs: DataScheduledJobs,
-    private readonly moduleRef: ModuleRef
+    private readonly moduleRef: ModuleRef,
+    private readonly prisma: PrismaService
   ) {}
 
   @Command({
@@ -39,5 +44,65 @@ export class DataCommands {
     })
     console.log("Running health check...")
     await this.dataJobs.checkAll(withDetails)
+  }
+
+  @Command({
+    command: "get:reservation-history <seasonsUID>",
+    describe: "get the reservation history for a given physical product",
+    aliases: "grh",
+  })
+  async getReservationHistory(
+    @Positional({
+      name: "seasonsUID",
+      type: "string",
+      describe: "seasonsUID of the item",
+    })
+    seasonsUID,
+    @PrismaEnvOption({
+      choices: ["local", "staging", "production"],
+    })
+    prismaEnv
+  ) {
+    await this.scriptsService.updateConnections({
+      prismaEnv,
+      moduleRef: this.moduleRef,
+    })
+    const physProd = await this.prisma.client.physicalProduct({ seasonsUID })
+    if (!physProd) {
+      this.logger.error(
+        `No Physical Product found with seasonsUID: ${seasonsUID}`
+      )
+    }
+    const reservations = await this.prisma.binding.query.reservations(
+      {
+        where: { products_some: { seasonsUID } },
+        orderBy: "createdAt_ASC",
+      },
+      `{
+        status
+        reservationNumber
+        createdAt
+        updatedAt
+        customer {
+          user {
+            email
+          }
+        }
+      }`
+    )
+    const formattedReservations = reservations.map(a => ({
+      ...pick(a, [
+        "reservationNumber",
+        "status",
+        "customer.user.email",
+        "createdAt",
+        "updatedAt",
+      ]),
+    }))
+    if (formattedReservations.length === 0) {
+      this.logger.log("Item has never been reserved")
+    }
+    this.logger.log(`*** Reservation History ***`)
+    this.logger.log(formattedReservations)
   }
 }
