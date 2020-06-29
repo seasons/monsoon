e53fe38da8261013ec70b74851e285d310ae763b
Kieran Gillen <kgillen@gmail.com>
2020-06-29 10:33:57 -0400

    Adds zipCode to createUser

--- src/modules/Scripts/commands/user.command.ts
+++ src/modules/Scripts/commands/user.command.ts
@@ -170,6 +170,7 @@ export class UserCommands {
         password,
         firstName,
         lastName,
+        zipCode: "10013",
         details: {
           phoneNumber: "(646) 350-2715",
           height: 40 + faker.random.number(32),
