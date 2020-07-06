556a45cb2d36adbf423b137834e6fefe28e02fa9
danielVebman <danveb800@gmail.com>
2020-07-06 15:44:51 -0400

    Remove SetPaymentDetails onboarding step

--- src/modules/Customer/fields/customer.fields.resolver.ts
+++ src/modules/Customer/fields/customer.fields.resolver.ts
@@ -123,9 +123,6 @@ export class CustomerFieldsResolver {
       .customer({ id: customer.id })
       .detail()
       .height()
-    const billingInfo = await this.prisma.client
-      .customer({ id: customer.id })
-      .billingInfo()
     const style = await this.prisma.client
       .customer({ id: customer.id })
       .detail()
@@ -139,14 +136,12 @@ export class CustomerFieldsResolver {
     const values = [
       verificationStatus === "Approved",
       height !== null,
-      billingInfo !== null,
       style !== null,
       shippingAddress !== null,
     ]
     const keys = [
       "VerifiedPhone",
       "SetMeasurements",
-      "SetPaymentDetails",
       "SetStylePreferences",
       "SetShippingAddress",
     ]
