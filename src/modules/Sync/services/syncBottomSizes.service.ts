c55524007dc213047a3d52b4a39c538e036d5c46
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-07-10 11:45:06 -0400

    cleans out sync bottom sizes

--- src/modules/Sync/services/syncBottomSizes.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@nestjs/common"
-
-@Injectable()
-export class SyncBottomSizesService {
-  constructor() {}
-
-  getBottomSizeRecordIdentifer = rec =>
-    `${rec.fields.Name}${rec.fields.Waist}${rec.fields.Rise}${rec.fields.Hem}${rec.fields.Inseam}`
-}
