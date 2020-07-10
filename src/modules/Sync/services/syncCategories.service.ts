8653463adfd776cd3e3fa5955cac4ef3335fb1fe
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-07-10 11:30:48 -0400

    removes category sync airtable to airtable

--- src/modules/Sync/services/syncCategories.service.ts
+++ src/modules/Sync/services/syncCategories.service.ts
@@ -5,7 +5,6 @@ import slugify from "slugify"
 import { CategoryCreateInput, CategoryUpdateInput } from "../../../prisma"
 import { PrismaService } from "../../../prisma/prisma.service"
 import { AirtableService } from "../../Airtable/services/airtable.service"
-import { UtilsService } from "../../Utils/services/utils.service"
 import { SyncUtilsService } from "./sync.utils.service"
 
 @Injectable()
@@ -13,43 +12,10 @@ export class SyncCategoriesService {
   constructor(
     private readonly airtableService: AirtableService,
     private readonly prisma: PrismaService,
-    private readonly syncUtils: SyncUtilsService,
-    private readonly utils: UtilsService
+    private readonly syncUtils: SyncUtilsService
   ) {}
 
   getCategoryRecordIdentifier = rec => rec.fields.Name
-
-  async syncAirtableToAirtable(cliProgressBar?) {
-    const allProductionCategories = await this.airtableService.getAllCategories(
-      this.airtableService.getProductionBase()
-    )
-    await this.syncUtils.deleteAllStagingRecords("Categories", cliProgressBar)
-
-    await this.syncUtils.createAllStagingRecordsWithoutLinks({
-      modelName: "Categories",
-      allProductionRecords: allProductionCategories,
-      sanitizeFunc: fields =>
-        this.utils.deleteFieldsFromObject(
-          {
-            ...fields,
-            Image: [],
-            Parent: [],
-            Products: [],
-          },
-          ["Products 2"]
-        ),
-      cliProgressBar,
-    })
-
-    const allStagingCategories = await this.airtableService.getAllCategories(
-      this.airtableService.getStagingBase()
-    )
-    await this.addParentCategoryLinks(
-      allProductionCategories,
-      allStagingCategories,
-      cliProgressBar
-    )
-  }
 
   async syncAirtableToPrisma(cliProgressBar?) {
     const allCategories = await this.airtableService.getAllCategories()
@@ -143,24 +109,6 @@ export class SyncCategoriesService {
     }
 
     multibar?.stop()
-  }
-
-  private async addParentCategoryLinks(
-    allProductionCategories,
-    allStagingCategories,
-    cliProgressBar?
-  ) {
-    await this.syncUtils.linkStagingRecords({
-      rootRecordName: "Categories",
-      targetFieldNameOnRootRecord: "Parent",
-      allRootProductionRecords: allProductionCategories,
-      allRootStagingRecords: allStagingCategories,
-      allTargetProductionRecords: allProductionCategories,
-      allTargetStagingRecords: allStagingCategories,
-      getRootRecordIdentifer: rec => rec.fields.Name,
-      getTargetRecordIdentifer: rec => rec.fields.Name,
-      cliProgressBar,
-    })
   }
 
   private buildHierarchy(items) {
