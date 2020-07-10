eda32faee6afe7e139421d742891b22d687ff60a
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-07-10 11:34:01 -0400

    removes physical product airtable airtable sync

--- src/modules/Sync/services/syncPhysicalProducts.service.ts
+++ src/modules/Sync/services/syncPhysicalProducts.service.ts
@@ -1,88 +1,23 @@
 import * as fs from "fs"
 
-import { PhysicalProductUpdateInput } from "@app/prisma"
 import { Injectable } from "@nestjs/common"
 import { isEmpty } from "lodash"
 
 import { PrismaService } from "../../../prisma/prisma.service"
-import { AirtableData } from "../../Airtable/airtable.types"
 import { AirtableService } from "../../Airtable/services/airtable.service"
 import { UtilsService } from "../../Utils/services/utils.service"
 import { SyncUtilsService } from "./sync.utils.service"
-import { SyncProductsService } from "./syncProducts.service"
-import { SyncProductVariantsService } from "./syncProductVariants.service"
 
 @Injectable()
 export class SyncPhysicalProductsService {
   constructor(
     private readonly airtableService: AirtableService,
     private readonly prisma: PrismaService,
-    private readonly syncProductsService: SyncProductsService,
-    private readonly syncProductVariantsService: SyncProductVariantsService,
     private readonly syncUtils: SyncUtilsService,
     private readonly utils: UtilsService
   ) {}
 
   getPhysicalProductRecordIdentifier = rec => rec.fields.SUID.text
-
-  async syncAirtableToAirtable(cliProgressBar?: any) {
-    const allPhysicalProductsProduction = await this.airtableService.getAllPhysicalProducts(
-      this.airtableService.getProductionBase()
-    )
-    await this.syncUtils.deleteAllStagingRecords(
-      "Physical Products",
-      cliProgressBar
-    )
-    await this.syncUtils.createAllStagingRecordsWithoutLinks({
-      modelName: "Physical Products",
-      allProductionRecords: allPhysicalProductsProduction,
-      sanitizeFunc: fields =>
-        this.utils.deleteFieldsFromObject(
-          {
-            ...fields,
-            Product: [],
-            Location: [],
-            "Product Variant": [],
-            Reservations: [],
-          },
-          [
-            "Created At",
-            "Updated At",
-            "Sequence Number",
-            "Item Weight",
-            "Barcode Image URL",
-            "Images",
-            "Barcode",
-            "Category",
-            "Type",
-            "Season",
-            "Brand",
-            "Retail Price",
-            "Architecture",
-            "Outer Material",
-            "Inner Materials",
-            "Display Category",
-            "Return Date",
-            "Created Date",
-            "Type Category",
-          ]
-        ),
-      cliProgressBar,
-    })
-    const allPhysicalProductsStaging = await this.airtableService.getAllPhysicalProducts(
-      this.airtableService.getStagingBase()
-    )
-    await this.addProductLinks(
-      allPhysicalProductsProduction,
-      allPhysicalProductsStaging,
-      cliProgressBar
-    )
-    await this.addProductVariantLinks(
-      allPhysicalProductsProduction,
-      allPhysicalProductsStaging,
-      cliProgressBar
-    )
-  }
 
   async syncAirtableToPrisma(cliProgressBar?) {
     const allProductVariants = await this.airtableService.getAllProductVariants()
@@ -149,51 +84,5 @@ export class SyncPhysicalProductsService {
 
     multibar?.stop()
     fs.closeSync(logFile)
-  }
-
-  private async addProductLinks(
-    allPhysicalProductsProduction: AirtableData,
-    allPhysicalProductsStaging: AirtableData,
-    cliProgressBar?: any
-  ) {
-    await this.syncUtils.linkStagingRecords({
-      rootRecordName: "Physical Products",
-      targetFieldNameOnRootRecord: "Product",
-      allRootProductionRecords: allPhysicalProductsProduction,
-      allRootStagingRecords: allPhysicalProductsStaging,
-      allTargetProductionRecords: await this.airtableService.getAllProducts(
-        this.airtableService.getProductionBase()
-      ),
-      allTargetStagingRecords: await this.airtableService.getAllProducts(
-        this.airtableService.getStagingBase()
-      ),
-      getRootRecordIdentifer: this.getPhysicalProductRecordIdentifier,
-      getTargetRecordIdentifer: this.syncProductsService
-        .getProductRecordIdentifer,
-      cliProgressBar,
-    })
-  }
-
-  private async addProductVariantLinks(
-    allPhysicalProductsProduction: AirtableData,
-    allPhysicalProductsStaging: AirtableData,
-    cliProgressBar?: any
-  ) {
-    await this.syncUtils.linkStagingRecords({
-      rootRecordName: "Physical Products",
-      targetFieldNameOnRootRecord: "Product Variant",
-      allRootProductionRecords: allPhysicalProductsProduction,
-      allRootStagingRecords: allPhysicalProductsStaging,
-      allTargetProductionRecords: await this.airtableService.getAllProductVariants(
-        this.airtableService.getProductionBase()
-      ),
-      allTargetStagingRecords: await this.airtableService.getAllProductVariants(
-        this.airtableService.getStagingBase()
-      ),
-      getRootRecordIdentifer: this.getPhysicalProductRecordIdentifier,
-      getTargetRecordIdentifer: this.syncProductVariantsService
-        .getProductVariantRecordIdentifier,
-      cliProgressBar,
-    })
   }
 }
