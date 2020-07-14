3a967d6ae2d4032c0bb4308e493df53e7af89af5
danielVebman <danveb800@gmail.com>
2020-07-14 15:36:14 -0400

    Finish customer weight migration script

--- src/scripts/migrateCustomerSizes.ts
+++ src/scripts/migrateCustomerSizes.ts
@@ -5,9 +5,21 @@ import { PrismaService } from "../prisma/prisma.service"
 
 const run = async () => {
   const ps = new PrismaService()
-  for (const customerDetail of await ps.client.customerDetails()) {
+
+  console.log("Fetching customer details from Prisma...")
+  const customerDetails = await ps.client.customerDetails()
+
+  let i = 0
+  const total = customerDetails.length
+
+  for (const customerDetail of customerDetails) {
+    console.log(`${i++} of ${total}`)
+
     const averageTopSize = customerDetail.averageTopSize?.trim()
-    const averageWaistSize = parseInt(customerDetail.averageTopSize?.trim(), 10)
+    const averageWaistSize = parseInt(
+      customerDetail.averageWaistSize?.trim(),
+      10
+    )
 
     const data: CustomerDetailUpdateInput = {}
     if (averageTopSize) {
