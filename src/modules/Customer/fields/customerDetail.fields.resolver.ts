2c971e65b792abff48d808826487f844a512e84a
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-07-20 16:14:33 -0400

    return waistSizes in size order

--- src/modules/Customer/fields/customerDetail.fields.resolver.ts
+++ src/modules/Customer/fields/customerDetail.fields.resolver.ts
@@ -7,11 +7,12 @@ export class CustomerDetailFieldsResolver {
 
   @ResolveField()
   async topSizes(@Parent() detail: CustomerDetail) {
-    if (!detail.topSizes) {
-      // we expect the data to be pulled in a parent resolver
-      return null
-    }
     const order = ["XXXS", "XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL"]
-    return detail.topSizes.sort((a, b) => order.indexOf(a) - order.indexOf(b))
+    return detail.topSizes?.sort((a, b) => order.indexOf(a) - order.indexOf(b))
+  }
+
+  @ResolveField()
+  async waistSizes(@Parent() detail: CustomerDetail) {
+    return detail.waistSizes?.sort()
   }
 }
