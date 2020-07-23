68e5c8987d0f2d10b7ec882487ba1c7148bf2ac5
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-07-23 12:34:15 -0400

    implements v1 ops gate

--- src/modules/User/services/admissions.ts
+++ src/modules/User/services/admissions.ts
@@ -1,8 +1,11 @@
 import * as fs from "fs"
 
-import { Customer } from "@app/prisma"
+import { Customer, EmailId } from "@app/prisma"
 import { Injectable } from "@nestjs/common"
 import { PrismaService } from "@prisma/prisma.service"
+import { uniqBy } from "lodash"
+import { DateTime } from "luxon"
+import moment from "moment"
 import zipcodes from "zipcodes"
 
 @Injectable()
@@ -20,9 +23,63 @@ export class AdmissionsService {
     return states.includes(state)
   }
 
-  // TODO: Write function
-  belowWeeklyNewUsersOpsThreshold(): boolean {
-    return false
+  // TODO: Test function. Consider reworking invitiations part
+  async belowWeeklyNewActiveUsersOpsThreshold(): Promise<boolean> {
+    const emailsSent = await this.prisma.binding.query.emailReceipts(
+      {
+        where: {
+          emailId_in: ["WelcomeToSeasons", "CompleteAccount", "PriorityAccess"],
+        },
+        orderBy: "createdAt_DESC",
+      },
+      `{
+        createdAt
+        user {
+          id
+        }
+      }`
+    )
+    const now = moment(new Date())
+    const emailsSentPastWeek = emailsSent.filter(a => {
+      const numDaysSinceEmailSent = now.diff(moment(a.createdAt))
+      return numDaysSinceEmailSent <= 6 // 0-6 is 7 days
+    })
+
+    // Find new users activated
+    const welcomeEmailsSentPastWeek = emailsSentPastWeek.filter(
+      a => a.emailId === "WelcomeToSeasons"
+    )
+    const usersActivatedPastWeek = uniqBy(
+      welcomeEmailsSentPastWeek,
+      a => a.user.id
+    ).map(a => a.user.id)
+
+    // Calculate number of invitations sent
+    const invitationsSentPastWeek = emailsSentPastWeek.filter(a =>
+      ["CompleteAccount", "PriorityAccess"].includes(a.emailId)
+    )
+    const usersInvitedButNotActivatedPastWeek = uniqBy(
+      invitationsSentPastWeek,
+      a => a.user.id
+    )
+      .map(b => b.user.id)
+      .filter(c => !usersActivatedPastWeek.includes(c))
+
+    if (
+      usersActivatedPastWeek.length >
+      parseInt(process.env.WEEKLY_NEW_USERS_THRESHOLD, 10)
+    ) {
+      return false
+    }
+
+    if (
+      usersInvitedButNotActivatedPastWeek.length >
+      parseInt(process.env.WEEKLY_INVITATIONS_THRESHOLD, 10)
+    ) {
+      return false
+    }
+
+    return true
   }
 
   // TODO: Write function
