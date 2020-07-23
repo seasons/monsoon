781e1f2da7494042e8f3dd7e62a13e4bfa5e45d8
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-07-23 17:20:47 -0400

    lay groundwork

--- src/modules/Product/fields/physicalProduct.fields.resolver.ts
+++ src/modules/Product/fields/physicalProduct.fields.resolver.ts
@@ -1,9 +1,12 @@
 import { Loader } from "@app/modules/DataLoader"
+import { PrismaService } from "@app/prisma/prisma.service"
 import { Parent, ResolveField, Resolver } from "@nestjs/graphql"
 import { PrismaDataLoader, PrismaLoader } from "@prisma/prisma.loader"
 
 @Resolver("PhysicalProduct")
 export class PhysicalProductFieldsResolver {
+  constructor(private readonly prisma: PrismaService) {}
+
   @ResolveField()
   async barcode(
     @Parent() physicalProduct,
@@ -15,8 +18,38 @@ export class PhysicalProductFieldsResolver {
         formatData: a => `SZNS` + `${a.sequenceNumber}`.padStart(5, "0"),
       },
     })
-    prismaLoader: PrismaDataLoader<string>
+    physicalProductsLoader: PrismaDataLoader<string>
   ) {
-    return prismaLoader.load(physicalProduct.id)
+    return physicalProductsLoader.load(physicalProduct.id)
+  }
+
+  @ResolveField()
+  async reservations(
+    @Parent() physicalProduct,
+    @Loader({
+      type: PrismaLoader.name,
+      params: {
+        query: "reservations",
+        infoFragment: `fragment EnsureProductIDs on Reservation {products {id}}`,
+        formatWhere: ids => {
+          where: {
+            products_some: {
+              id_in: ids
+            }
+          }
+        },
+      },
+      includeInfo: true,
+    })
+    reservationsLoader: PrismaDataLoader<string>
+  ) {
+    debugger
+    const reservations = (await reservationsLoader.load(
+      physicalProduct.id
+    )) as any
+    console.log(reservations)
+    return reservations.filter(a =>
+      a.products.map(b => b.id).includes(physicalProduct.id)
+    )
   }
 }
