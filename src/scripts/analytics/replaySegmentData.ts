54248f5a1de16b239f75fcbf0e569877b68871ab
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-08-28 16:37:33 -0400

    full monsoon replay script complete

--- src/scripts/analytics/replaySegmentData.ts
+++ src/scripts/analytics/replaySegmentData.ts
@@ -6,14 +6,11 @@ import Analytics from "analytics-node"
 import csv from "csv-parser"
 import { camelCase, mapKeys, pick } from "lodash"
 
+import {
+  CustomerWhereInput,
+  EmailReceiptWhereInput,
+} from "../../prisma/prisma.binding"
 import { PrismaService } from "../../prisma/prisma.service"
-
-const ps = new PrismaService()
-
-// Useful Docs: https://segment.com/docs/connections/sources/catalog/libraries/server/node/#identify
-
-const monsoonStagingWriteKey = ""
-const client = new Analytics(monsoonStagingWriteKey)
 
 interface TrackPayload {
   properties: any
@@ -24,6 +21,25 @@ interface IdentifyPayload {
 }
 
 type SegmentPayload = TrackPayload | IdentifyPayload
+
+interface ReplayEventsFromDBRecords {
+  query: string
+  getUserIdFunc: (rec) => string
+  createPayloadFunc: (rec) => any
+  getTimestampFunc?: (rec) => Date
+  shouldSendEventFunc?: (rec) => boolean
+  where?: any
+  event?: string
+  method?: "identify" | "track"
+  info: any
+}
+
+const ps = new PrismaService()
+
+// Useful Docs: https://segment.com/docs/connections/sources/catalog/libraries/server/node/#identify
+
+const monsoonStagingWriteKey = ""
+const client = new Analytics(monsoonStagingWriteKey)
 
 const uploadCSV = ({
   csvName,
@@ -54,130 +70,184 @@ const uploadCSV = ({
     .createReadStream(`events/monsoon/${csvName}.csv`)
     .pipe(csv())
     .on("data", async row => {
-      //   if (i <= 2) {
-      console.log(`row ${i++}`)
-      pipe.pause()
-      client[method]({
-        userId: getUserIDFunc(row),
-        ...rowTransformFunc(row),
-        ...fieldsToAddToPayloads,
-        timestamp: new Date(row.sent_at),
-      })
-      pipe.resume()
-      //   }
+      if (i <= 2) {
+        console.log(`row ${i++}`)
+        pipe.pause()
+        const payload = {
+          userId: getUserIDFunc(row),
+          ...rowTransformFunc(row),
+          ...fieldsToAddToPayloads,
+          timestamp: new Date(row.sent_at),
+        }
+        client[method](payload)
+        pipe.resume()
+      }
     })
     .on("end", () => {
       console.log("CSV file successfully processed")
     })
 }
 
-const createPropertiesFromColumns = (row, columnNames: string[]) =>
-  mapKeys(pick(row, columnNames), camelCase)
+const createPropertiesFromCSVColumns = (row, columnNames: string[]) => {
+  const x = pick(row, columnNames)
+  const props = mapKeys(x, (val, key) => camelCase(key))
+  return props
+}
 
-const replayCreatedAccountsFromDBRecords = async () => {
-  const allCustomers = await ps.binding.query.customers(
-    {},
-    `{
-      id
-      detail {
-        phoneNumber
+const replayEventFromDBRecords = async ({
+  query,
+  getUserIdFunc,
+  createPayloadFunc,
+  getTimestampFunc = rec => new Date(rec.createdAt),
+  shouldSendEventFunc = rec => true,
+  where = {},
+  event,
+  method = "track",
+  info,
+}: ReplayEventsFromDBRecords) => {
+  if (method === "identify" && !!event) {
+    throw new Error("Can not pass event on identify call")
+  }
+  if (method === "track" && !event) {
+    throw new Error("Must pass event on track call")
+  }
+
+  try {
+    const records = await ps.binding.query[query](where, info)
+
+    const createCommonFields = rec => ({
+      userId: getUserIdFunc(rec),
+      integrations: { All: false, Mixpanel: true },
+      timestamp: getTimestampFunc(rec),
+      ...(!!event ? { event } : {}),
+    })
+
+    const payloadKey = method === "identify" ? "traits" : "properties"
+    let i = 0
+    for (const rec of records) {
+      if (i > 5) {
+        break
       }
+      console.log(
+        `Record ${i++} of ${(records as any[]).length} for ${
+          method === "track" ? `event: ${event}` : "identify"
+        }`
+      )
+      if (shouldSendEventFunc(rec)) {
+        const payload = {
+          ...createCommonFields(rec),
+          [payloadKey]: createPayloadFunc(rec),
+        }
+        client[method](payload)
+      }
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const getNameAndEmailFromUser = user =>
+  pick(user, ["firstName", "lastName", "email"])
+
+const replayMonsoonData = async () => {
+  // Created Account and Identify
+  const CreatedAccountAndIdentifyCommonParams = {
+    query: "customers",
+    info: `{
+            id
+            detail {
+              phoneNumber
+            }
+            user {
+              id
+              email
+              firstName
+              lastName
+              auth0Id
+              roles
+              createdAt
+            }
+          }`,
+    getUserIdFunc: rec => rec.user.id,
+    getTimestampFunc: rec => new Date(rec.user.createdAt),
+  } as Partial<ReplayEventsFromDBRecords>
+  await replayEventFromDBRecords({
+    ...CreatedAccountAndIdentifyCommonParams,
+    method: "identify",
+    createPayloadFunc: rec => ({
+      ...pick(rec.user, [
+        "firstName",
+        "lastName",
+        "id",
+        "email",
+        "auth0Id",
+        "roles",
+        "createdAt",
+      ]),
+      phone: rec.detail.phoneNumber,
+    }),
+  } as ReplayEventsFromDBRecords)
+  await replayEventFromDBRecords({
+    ...CreatedAccountAndIdentifyCommonParams,
+    event: "Created Account",
+    createPayloadFunc: rec => ({
+      name: `${rec.user.firstName} ${rec.user.lastName}`,
+      ...getNameAndEmailFromUser(rec.user),
+      customerID: rec.id,
+      application: "harvest",
+    }),
+  } as ReplayEventsFromDBRecords)
+
+  // Reserved Items
+  await replayEventFromDBRecords({
+    query: "reservations",
+    info: `{
+      id
       user {
         id
         email
         firstName
         lastName
-        auth0Id
-        roles
       }
-    }`
-  )
-
-  const createCommonFields = user => ({
-    userId: user.id,
-    integrations: { All: false, Mixpanel: true },
-    timestamp: new Date(user.createdAt),
-  })
-
-  for (const cust of allCustomers) {
-    client.identify({
-      ...createCommonFields(cust.user),
-      traits: {
-        ...pick(cust.user, [
-          "firstName",
-          "lastName",
-          "id",
-          "email",
-          "auth0Id",
-          "roles",
-          "createdAt",
-        ]),
-        phone: cust.detail.phoneNumber,
-      },
-    })
-
-    client.track({
-      ...createCommonFields(cust.user),
-      properties: {
-        name: `${cust.user.firstName} ${cust.user.lastName}`,
-        ...pick(cust.user, ["firstName", "lastName", "email"]),
-        customerID: cust.id,
-        application: "harvest",
-      },
-    })
-  }
-}
-
-const replayMonsoonData = () => {
-  uploadCSV({
-    csvName: "identifies",
-    method: "identify",
-    rowTransformFunc: row => ({
-      traits: {
-        auth0Id: row.auth0_id,
-        ...createPropertiesFromColumns(row, [
-          "email",
-          "last_name",
-          "phone",
-          "first_name",
-          "roles",
-        ]),
-      },
-    }),
-  })
-
-  uploadCSV({
-    csvName: "created_account",
-    event: "Created Account",
-    rowTransformFunc: row => ({
-      properties: {
-        customerID: row.customer_id,
-        ...createPropertiesFromColumns(row, [
-          "name",
-          "last_name",
-          "email",
-          "first_name",
-        ]),
-      },
-    }),
-  })
-
-  uploadCSV({
-    csvName: "reserved_items",
+      products {
+        seasonsUID
+        productVariant {
+          id
+        }
+      }
+      createdAt
+    }`,
+    getUserIdFunc: rec => rec.user.id,
     event: "Reserved Items",
-    rowTransformFunc: row => ({
-      properties: {
-        reservationID: row.reservation_id,
-        ...createPropertiesFromColumns(row, ["email", "units", "items"]),
-      },
+    createPayloadFunc: rec => ({
+      ...getNameAndEmailFromUser(rec.user),
+      reservationID: rec.id,
+      items: rec.products.map(a => a.productVariant.id),
+      units: rec.products.map(a => a.seasonsUID),
+      application: "harvest",
     }),
   })
 
+  // Became Authorized
+  await replayEventFromDBRecords({
+    query: "emailReceipts",
+    where: { where: { emailId: "CompleteAccount" } as EmailReceiptWhereInput },
+    info: `{user {id firstName lastName email} createdAt}`,
+    getUserIdFunc: rec => rec.user.id,
+    event: "Became Authorized",
+    createPayloadFunc: rec => ({
+      ...getNameAndEmailFromUser(rec.user),
+      method: "Manual",
+    }),
+  })
+
+  // We have no records on whether or not someone opened a hosted checkout
+  // besides the actual event data, so we leave this one as is.
   uploadCSV({
     csvName: "opened_hosted_checkout",
     event: "Opened Hosted Checkout",
     rowTransformFunc: row => ({
-      properties: createPropertiesFromColumns(row, [
+      properties: createPropertiesFromCSVColumns(row, [
         "plan",
         "last_name",
         "email",
