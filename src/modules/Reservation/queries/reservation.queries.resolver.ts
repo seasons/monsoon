be6ced7fdd9c1727514faacadd117a63eecf14a1
Luc Succes <l2succes@gmail.com>
2020-09-08 11:55:59 +0200

    Use Phase to filter reservations

--- src/modules/Reservation/queries/reservation.queries.resolver.ts
+++ src/modules/Reservation/queries/reservation.queries.resolver.ts
@@ -1,3 +1,4 @@
+import { ReservationWhereInput } from "@app/prisma"
 import { Args, Info, Query, Resolver } from "@nestjs/graphql"
 import { PrismaService } from "@prisma/prisma.service"
 
@@ -27,7 +28,37 @@ export class ReservationQueriesResolver {
 
   @Query()
   async reservations(@Args() args, @Info() info) {
-    const data = await this.prisma.binding.query.reservations(args, info)
+    const { where } = args
+    const options: ReservationWhereInput = {}
+
+    if (where.phase) {
+      const clause = {
+        slug: process.env.SEASONS_CLEANER_LOCATION_SLUG,
+      }
+      switch (where.phase) {
+        case "CustomerToBusiness":
+          options.AND = {
+            status_not: "Completed",
+            lastLocation: {
+              NOT: clause,
+            },
+          }
+          break
+        case "BusinessToCustomer":
+          options.lastLocation = clause
+          break
+      }
+    }
+    const data = await this.prisma.binding.query.reservations(
+      {
+        ...args,
+        where: {
+          ...args.where,
+          ...options,
+        },
+      },
+      info
+    )
     return data
   }
 
