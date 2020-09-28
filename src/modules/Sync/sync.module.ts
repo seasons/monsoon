4285c97896ce2b6f523fda9f80a1d741eb7e7f0f
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-09-28 18:42:16 -0400

    removes airtable prisma sync

--- src/modules/Sync/sync.module.ts
+++ src/modules/Sync/sync.module.ts
@@ -1,49 +1,15 @@
 import { Module } from "@nestjs/common"
 
 import { PrismaModule } from "../../prisma/prisma.module"
-import { AirtableModule } from "../Airtable/airtable.module"
 import { ImageModule } from "../Image/image.module"
 import { ProductModule } from "../Product/product.module"
 import { UserModule } from "../User/user.module"
 import { UtilsModule } from "../Utils/utils.module"
-import { AirtableSyncService } from "./services/sync.airtable.service"
 import { PrismaSyncService } from "./services/sync.prisma.service"
-import { SyncUtilsService } from "./services/sync.utils.service"
-import { SyncBrandsService } from "./services/syncBrands.service"
-import { SyncCategoriesService } from "./services/syncCategories.service"
-import { SyncCollectionGroupsService } from "./services/syncCollectionGroups.service"
-import { SyncCollectionsService } from "./services/syncCollections.service"
-import { SyncColorsService } from "./services/syncColors.service"
-import { SyncHomepageProductRailsService } from "./services/syncHomepageProductRails.service"
-import { SyncModelsService } from "./services/syncModels.service"
-import { SyncPhysicalProductsService } from "./services/syncPhysicalProducts.service"
-import { SyncProductsService } from "./services/syncProducts.service"
-import { SyncProductVariantsService } from "./services/syncProductVariants.service"
 
 @Module({
-  imports: [
-    AirtableModule,
-    ImageModule,
-    PrismaModule,
-    ProductModule,
-    UserModule,
-    UtilsModule,
-  ],
-  exports: [AirtableSyncService, PrismaSyncService],
-  providers: [
-    AirtableSyncService,
-    PrismaSyncService,
-    SyncBrandsService,
-    SyncCategoriesService,
-    SyncCollectionGroupsService,
-    SyncCollectionsService,
-    SyncColorsService,
-    SyncHomepageProductRailsService,
-    SyncModelsService,
-    SyncPhysicalProductsService,
-    SyncProductsService,
-    SyncProductVariantsService,
-    SyncUtilsService,
-  ],
+  imports: [ImageModule, PrismaModule, ProductModule, UserModule, UtilsModule],
+  exports: [PrismaSyncService],
+  providers: [PrismaSyncService],
 })
 export class SyncModule {}
