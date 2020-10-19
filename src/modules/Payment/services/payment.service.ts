a277e6a8970eae7d1779bfdee496cfb1bc2d9769
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-10-19 16:16:42 -0400

    if a customer has a payment method during apple pay checkout, delete the old one before creating the new one

--- src/modules/Payment/services/payment.service.ts
+++ src/modules/Payment/services/payment.service.ts
@@ -223,13 +223,25 @@ export class PaymentService {
         .request()
     }
 
-    const paymentSource = await chargebee.payment_source
-      .create_using_temp_token({
-        tmp_token: token.tokenId,
-        type: tokenType ? tokenType : "apple_pay",
-        customer_id: chargebeeCustomer.customer.id,
-      })
-      .request()
+    // Create the payment method. If one already exists, delete it
+    // and create a new one using the passed token.
+    let paymentSource
+    const customerPaymentSources = await chargebee.payment_source.list({
+      "customer_id[is]": chargebeeCustomer.customer.id,
+    })
+    paymentSource = head(customerPaymentSources.list)
+    if (!!paymentSource) {
+      await chargebee.payment_source.delete(paymentSource.payment_source.id)
+    }
+    if (!paymentSource) {
+      paymentSource = await chargebee.payment_source
+        .create_using_temp_token({
+          tmp_token: token.tokenId,
+          type: tokenType ? tokenType : "apple_pay",
+          customer_id: chargebeeCustomer.customer.id,
+        })
+        .request()
+    }
 
     const subscription = await chargebee.subscription
       .create_for_customer(chargebeeCustomer.customer.id, {
