9b520e8009241d7ddb7df06adfd3a58e38ed7c12
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-10-28 10:54:38 -0400

    adds authorizationsCount

--- src/scripts/runQuery.ts
+++ src/scripts/runQuery.ts
@@ -5,10 +5,38 @@ import { PrismaService } from "../prisma/prisma.service"
 const run = async () => {
   const ps = new PrismaService()
 
-  await ps.client.updateManyCustomers({
-    where: { status: "Authorized" },
-    data: { status: "Waitlisted" },
-  })
+  const triageCustomerInfo = `{
+    id
+    status
+    detail {
+      shippingAddress {
+        zipCode
+      }
+      topSizes
+      waistSizes
+    }
+    user {
+      id
+      firstName
+      lastName
+      email
+      emails {
+        emailId
+      }
+    }
+    admissions {
+      authorizationsCount
+    }
+  }`
+  const x = await ps.binding.query.customers(
+    {
+      where: {
+        status_in: ["Invited", "Created", "Waitlisted", "Authorized"],
+      },
+    },
+    triageCustomerInfo
+  )
+  console.log(x)
 }
 
 run()
