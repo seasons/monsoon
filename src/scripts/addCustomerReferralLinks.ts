186f8bf0e78c55dfee2e31bb65c86f1ebe670d5f
Faiyam Rahman <faiyam@faiyamrahman.com>
2020-11-06 18:01:13 -0500

    Revert "minor adjustment to slash tag creation"

--- src/scripts/addCustomerReferralLinks.ts
+++ src/scripts/addCustomerReferralLinks.ts
@@ -1,5 +1,6 @@
 import "module-alias/register"
 
+import { get } from "lodash"
 import request from "request"
 
 import { PrismaService } from "../prisma/prisma.service"
@@ -14,31 +15,23 @@ const run = async () => {
     user {
       firstName
     }
-    referralLink
     }`
   )
-  const customersWithoutReferralLinks = customers.filter(
-    a => a.referralLink == null
-  )
-  const customersWithReferralLinks = customers.filter(
-    a => a.referralLink != null
-  )
-  let allSlashTags = customersWithReferralLinks.map(a => {
-    const link = a.referralLink
-    return link.replace("szns.co/", "").toLowerCase()
-  })
 
+  const seenFirstNames = {}
   let count = 0
-  let slashTag
-  for (const customer of customersWithoutReferralLinks) {
+
+  for (const customer of customers) {
     console.log(`customer ${count} of ${customers.length}`)
     if (count % 10 === 0) {
       // Because of rebrandly rate limiter
       await new Promise(resolve => setTimeout(resolve, 5000))
     }
     try {
-      //@ts-ignore
-      ;[slashTag, allSlashTags] = getSlashTag(customer, allSlashTags)
+      const firstName = customer.user.firstName.trim()
+      const appendedNumber: number = (get(seenFirstNames, firstName) ?? 0) + 1
+      seenFirstNames[firstName] = appendedNumber
+      const slashTag = firstName + appendedNumber.toString()
       await fetchAndSetCustomerReferralLink(customer.id, slashTag)
     } catch (e) {
       console.log("error in try catch: ", e)
@@ -46,24 +39,6 @@ const run = async () => {
     }
     count += 1
   }
-}
-
-const getSlashTag = (customer, allSlashTags) => {
-  /*
-  Check if the default slash tag is already taken. 
-
-  If it's not, return it along with an updated allSlashTags object
-
-  If it is, keep incrementing the count until we're good
-  */
-  const firstName = customer.user.firstName.trim().replace(/[^a-z]/gi, "")
-  let number = 1
-  let slashTag = `${firstName}${number}`.toLowerCase().replace(" ", "")
-  while (allSlashTags.includes(slashTag)) {
-    number++
-    slashTag = `${firstName}${number}`.toLowerCase().replace(" ", "")
-  }
-  return [slashTag, [...allSlashTags, slashTag]]
 }
 
 const fetchAndSetCustomerReferralLink = async (customerId, slashTag) => {
@@ -79,24 +54,19 @@ const fetchAndSetCustomerReferralLink = async (customerId, slashTag) => {
     workspace: process.env.REBRANDLY_WORKSPACE_ID,
   }
 
-  await request(
+  request(
     {
       uri: "https://api.rebrandly.com/v1/links",
       method: "POST",
       body: JSON.stringify(linkRequest),
       headers: requestHeaders,
     },
-    async (err, response, body) => {
-      if (body?.message === "Number of links exceeded") {
-        throw new Error(body)
-      }
+    (err, response, body) => {
       let link = JSON.parse(body)
-      await ps.binding.mutation.updateCustomer({
+      ps.binding.mutation.updateCustomer({
         data: { referralLink: link.shortUrl },
         where: { id: customerId },
       })
-      console.log(`updated customer: ${customerId} with link: ${link.shortUrl}`)
-      console.log(body)
     }
   )
 }
