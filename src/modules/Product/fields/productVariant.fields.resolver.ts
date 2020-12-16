9001f9cd22edd7583fbdb07fdb63eaa1cc35a389
Kieran Gillen <kgillen@gmail.com>
2020-12-16 15:32:45 -0500

    Adds resolver for display

--- src/modules/Product/fields/productVariant.fields.resolver.ts
+++ src/modules/Product/fields/productVariant.fields.resolver.ts
@@ -1,4 +1,7 @@
 import { Customer, User } from "@app/decorators"
+import { Loader } from "@app/modules/DataLoader/decorators/dataloader.decorator"
+import { ProductVariant } from "@app/prisma"
+import { PrismaDataLoader } from "@app/prisma/prisma.loader"
 import { Parent, ResolveField, Resolver } from "@nestjs/graphql"
 import { PrismaService } from "@prisma/prisma.service"
 import { head } from "lodash"
@@ -6,6 +9,81 @@ import { head } from "lodash"
 @Resolver("ProductVariant")
 export class ProductVariantFieldsResolver {
   constructor(private readonly prisma: PrismaService) {}
+
+  @ResolveField()
+  async display(
+    @Parent() parent,
+    @Loader({
+      params: {
+        query: "productVariants",
+        info: `
+        {
+          id
+          internalSize {
+            id
+            display
+            top {
+              id
+              letter
+            }
+            bottom {
+              id
+              value
+            }
+          }
+          manufacturerSizes {
+            id
+            display
+            top {
+              id
+              letter
+            }
+            bottom {
+              id
+              value
+            }
+          }
+        }`,
+      },
+    })
+    productVariantLoader: PrismaDataLoader<ProductVariant>
+  ) {
+    const loadedProductVariant = await productVariantLoader.load(parent.id)
+
+    console.log("loadedProductVariant", loadedProductVariant)
+
+    const sizeToName = size => {
+      switch (size) {
+        case "XXS":
+          return "XX-Small"
+        case "XS":
+          return "X-Small"
+        case "S":
+          return "Small"
+        case "M":
+          return "Medium"
+        case "L":
+          return "Large"
+        case "XL":
+          return "X-Large"
+        case "XXL":
+          return "XX-Large"
+        default:
+          return size
+      }
+    }
+
+    let long = ""
+    let short = ""
+    if (parent?.manufacturerSizes?.length > 0) {
+      long = sizeToName(parent?.manufacturerSizes[0].display)
+      short = sizeToName(parent?.manufacturerSizes[0].display)
+    } else if (parent?.internalSize?.display) {
+      long = sizeToName(parent?.internalSize?.display)
+      short = sizeToName(parent?.internalSize?.display)
+    }
+    return { long, short }
+  }
 
   @ResolveField()
   async isInBag(@Parent() parent, @Customer() customer) {
