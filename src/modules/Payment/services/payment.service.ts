779939c526dc6b0b8567432e7898d0c1ef8178f7
Luc Succes <l2succes@gmail.com>
2020-12-24 15:33:31 -0500

    Adds gift related queries

--- src/modules/Payment/services/payment.service.ts
+++ src/modules/Payment/services/payment.service.ts
@@ -579,6 +579,30 @@ export class PaymentService {
       .request()
   }
 
+  async getGift(id) {
+    return await chargebee.gift.retrieve(id).request()
+  }
+
+  async getGiftCheckoutPage(planId) {
+    return await new Promise((resolve, reject) => {
+      chargebee.hosted_page
+        .checkout_gift({
+          subscription: {
+            plan_id: planId,
+          },
+        })
+        .request((error, result) => {
+          if (error) {
+            reject(error)
+          } else {
+            resolve(result.hosted_page)
+          }
+        })
+    }).catch(error => {
+      throw new Error(JSON.stringify(error))
+    })
+  }
+
   async getHostedCheckoutPage(
     planId,
     userId,
@@ -601,7 +625,8 @@ export class PaymentService {
             last_name: lastName,
             phone: phoneNumber,
           },
-          redirect_url: "https://seasons.nyc/chargebee-mobile-checkout-success",
+          redirect_url:
+            "https://wearseasons.com/chargebee-mobile-checkout-success",
           coupon_ids: !!couponId ? [couponId] : [],
         })
         .request((error, result) => {
@@ -668,7 +693,8 @@ export class PaymentService {
     chargebeeCustomer: any,
     card: any,
     planID: string,
-    subscriptionID: string
+    subscriptionID: string,
+    giftID?: string
   ) {
     // Retrieve plan and billing data
     const billingInfo = this.paymentUtils.createBillingInfoObject(
@@ -701,6 +727,7 @@ export class PaymentService {
     await this.prisma.client.createCustomerMembership({
       customer: { connect: { id: prismaCustomer.id } },
       subscriptionId: subscriptionID,
+      giftId: giftID,
       plan: { connect: { planID } },
     })
 
