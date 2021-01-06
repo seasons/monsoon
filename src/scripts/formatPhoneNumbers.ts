0dc50be54003d4011851b3a5cea69835577f60ad
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-01-06 16:08:30 -0600

    remove dashes from phone numbers

--- src/scripts/formatPhoneNumbers.ts
+++ src/scripts/formatPhoneNumbers.ts
@@ -2,8 +2,6 @@ import "module-alias/register"
 
 import { PrismaService } from "../prisma/prisma.service"
 
-const phone = require("phone")
-
 const run = async () => {
   const ps = new PrismaService()
 
@@ -21,6 +19,9 @@ const run = async () => {
   let numErrors = 0
   for (const cust of customers) {
     const formattedPhone = cust.detail?.phoneNumber?.replace(/-/g, "")
+    if (formattedPhone !== cust.detail?.phoneNumber) {
+      console.log(`change: ${cust.detail?.phoneNumber}`)
+    }
     await ps.client.updateCustomerDetail({
       where: { id: cust.detail?.id },
       data: { phoneNumber: formattedPhone },
