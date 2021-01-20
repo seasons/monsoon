438c8a74661c18921c2bf187c1b408bce43e3b59
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-01-20 14:39:51 -0600

    adds script to followup with ~350 users who need it

--- src/scripts/runQuery.ts
+++ src/scripts/runQuery.ts
@@ -1,5 +1,7 @@
 import "module-alias/register"
 
+import * as util from "util"
+
 import { UtilsService } from "../modules/Utils/services/utils.service"
 import { PrismaService } from "../prisma/prisma.service"
 
@@ -7,36 +9,50 @@ const run = async () => {
   const ps = new PrismaService()
   const utils = new UtilsService(ps)
 
-  const authorizedCustomers = await ps.binding.query.customers({
-    where: { status: "Authorized" },
-  })
-  console.log(authorizedCustomers.length)
-  // const unhandledCustomers = await ps.binding.query.customers(
-  //   {
-  //     where: {
-  //       AND: [
-  //         { status: "Authorized" },
-  //         { user: { emails_none: { emailId: "Rewaitlisted" } } },
-  //         { admissions: { authorizationWindowClosesAt_lte: new Date() } },
-  //         { user: { createdAt_gte: new Date(2020, 9, 5) } },
-  //       ],
-  //     },
-  //   },
-  //   `{
-  //   id
-  //   user {
-  //     email
-  //     createdAt
-  //   }
-  // }`
-  // )
-  // let sortedCustomers = unhandledCustomers.sort(
-  //   (a, b) => utils.dateSort(a.user.createdAt, b.user.createdAt) * -1
-  // )
+  // const authorizedCustomers = await ps.binding.query.customers({
+  //   where: { status: "Authorized" },
+  // })
+  // console.log(authorizedCustomers.length)
+  const unhandledCustomers = await ps.binding.query.customers(
+    {
+      where: {
+        AND: [
+          { status: "Authorized" },
+          { user: { emails_none: { emailId: "Rewaitlisted" } } },
+          {
+            user: {
+              emails_none: { emailId: "TwentyFourHourAuthorizationFollowup" },
+            },
+          },
+          {
+            admissions: {
+              authorizationWindowClosesAt_lte: new Date(2021, 0, 21),
+            },
+          },
+        ],
+      },
+    },
+    `{
+    id
+    user {
+      email
+      createdAt
+    }
+  }`
+  )
+  let sortedCustomers = unhandledCustomers.sort(
+    (a, b) => utils.dateSort(a.user.createdAt, b.user.createdAt) * -1
+  )
   // console.log(sortedCustomers.length)
   // for (const c of sortedCustomers) {
   //   console.log(c)
   // }
+  const a = sortedCustomers.map(a => a.user.email)
+  for (const b of a) {
+    console.log(b)
+  }
+  // console.log(util.inspect(a, { depth: null, showHidden: true }))
+  // console.log(sortedCustomers.length)
 }
 
 run()
