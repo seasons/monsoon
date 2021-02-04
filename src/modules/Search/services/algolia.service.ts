e74fd79656b43113050b1d74b3d7cc75e8cf1340
Luc Succes <l2succes@gmail.com>
2021-02-04 15:34:24 -0500

    Update Algolia

--- src/modules/Search/services/algolia.service.ts
+++ src/modules/Search/services/algolia.service.ts
@@ -3,14 +3,52 @@ import algoliasearch, { SearchIndex } from "algoliasearch"
 
 const { ALGOLIA_ACCOUNT_ID, ALGOLIA_KEY } = process.env
 
-const MASTER_INDEX = "dev_inventory"
+export enum IndexKey {
+  Default = "default",
+  Admin = "dev_inventory",
+  Customer = "seasons_external",
+}
 
 @Injectable()
 export class AlgoliaService {
   client = algoliasearch(ALGOLIA_ACCOUNT_ID, ALGOLIA_KEY)
-  index: SearchIndex
+  defaultIndex: SearchIndex
+  indices: Map<IndexKey, SearchIndex> = new Map()
 
   constructor() {
-    this.index = this.client.initIndex(MASTER_INDEX)
+    this.defaultIndex = this.client.initIndex(IndexKey.Admin)
+    this.indices.set(IndexKey.Default, this.defaultIndex)
+    this.indices.set(IndexKey.Admin, this.defaultIndex)
+  }
+
+  getIndex(indexKey: IndexKey) {
+    if (this.indices.has(indexKey)) {
+      return this.indices.get(indexKey)
+    }
+
+    const newIndex = this.client.initIndex(indexKey)
+    this.indices.set(indexKey, newIndex)
+
+    return newIndex
+  }
+
+  async reindex(data, indexKeys: IndexKey[] = [IndexKey.Default]) {
+    const results = []
+
+    for (const indexKey of indexKeys) {
+      const index = this.getIndex(indexKey)
+
+      const result = await index.saveObjects(data, {
+        autoGenerateObjectIDIfNotExist: false,
+      })
+
+      results.push(result)
+    }
+
+    if (results.length === 1) {
+      return results[0]
+    }
+
+    return results
   }
 }
