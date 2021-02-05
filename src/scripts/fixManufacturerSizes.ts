943e105867ea5396087a44a0b37345964c820a40
Kieran Gillen <kgillen@gmail.com>
2021-02-05 16:29:31 -0500

    Fixes display long

--- src/scripts/fixManufacturerSizes.ts
+++ src/scripts/fixManufacturerSizes.ts
@@ -8,9 +8,9 @@ const run = async () => {
   const sizes = await ps.client.sizes()
 
   for (const size of sizes) {
-    // Fix for Letter sizes
-    if (size.display.includes("US ")) {
-      const newDisplay = size.display.replace("US ", "")
+    // Fix for JP sizes
+    if (size.display.includes("JP ")) {
+      const newDisplay = size.display.replace("JP ", "")
       await ps.client.updateSize({
         where: { id: size.id },
         data: {
@@ -23,9 +23,9 @@ const run = async () => {
   const variants = await ps.client.productVariants()
 
   for (const variant of variants) {
-    // Fix for Letter sizes
-    if (variant.displayShort.includes("US ")) {
-      const newDisplay = variant.displayShort.replace("US ", "")
+    // Fix for JP sizes
+    if (variant.displayShort.includes("JP ")) {
+      const newDisplay = variant.displayShort.replace("JP ", "")
       await ps.client.updateProductVariant({
         where: { id: variant.id },
         data: {
