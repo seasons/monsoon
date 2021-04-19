e134dcaf126ac97eb13dd7ba6503563ebd734773
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-04-19 17:53:45 -0400

    throw errors on style code collisions

--- src/modules/Product/services/product.service.ts
+++ src/modules/Product/services/product.service.ts
@@ -643,6 +643,12 @@ export class ProductService {
     }
 
     const styleCode = styleNumber.toString().padStart(3, "0")
+    const allStyleCodesForBrand = await this.productUtils.getAllStyleCodesForBrand(
+      brandID
+    )
+    if (!productID && allStyleCodesForBrand.includes(styleCode)) {
+      throw new Error(`Style code collision: ${styleCode}`)
+    }
     return {
       brandCode: brand.brandCode,
       styleCode,
