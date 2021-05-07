b2850101c76bbd3ec10e8cbdb1dbd96f5f1deb09
Kieran Gillen <kgillen@gmail.com>
2021-05-07 12:11:57 -0700

    Fixes content in webflow hook

--- src/modules/Blog/controllers/webflow.controller.ts
+++ src/modules/Blog/controllers/webflow.controller.ts
@@ -23,48 +23,61 @@ export class WebflowController {
     })
 
     const lastPostStored = head(firstBlogPosts) as any
+    const blogItemWithContent = await this.blog.getItem(lastPostPublished.id)
+    const content = blogItemWithContent["post-content"]
+
+    const blogData = {
+      content,
+      webflowId: lastPostPublished.id,
+      webflowCreatedAt: lastPostPublished.createdAt,
+      webflowUpdatedAt: lastPostPublished.updatedAt,
+      tags: { set: lastPostPublished.tags },
+      ...pick(lastPostPublished, [
+        "name",
+        "body",
+        "summary",
+        "thumbnailURL",
+        "imageURL",
+        "imageAlt",
+        "url",
+        "publishedOn",
+        "author",
+        "category",
+        "slug",
+      ]),
+    } as BlogPostCreateInput
+
+    const imageName = `${blogData.slug}-image`
+    const title = `${blogData.name} Image`
+    const alt = blogData.imageAlt ? blogData.imageAlt : title
+
+    const imageData = await this.image.uploadImageFromURL(
+      blogData.imageURL,
+      imageName,
+      imageName
+    )
+
+    const blogImage = await this.prisma.client.upsertImage({
+      where: { url: imageData.url },
+      create: { ...imageData, title, alt },
+      update: { ...imageData, title, alt },
+    })
 
     if (lastPostPublished.id !== lastPostStored.webflowId) {
-      const createData = {
-        webflowId: lastPostPublished.id,
-        webflowCreatedAt: lastPostPublished.createdAt,
-        webflowUpdatedAt: lastPostPublished.updatedAt,
-        tags: { set: lastPostPublished.tags },
-        ...pick(lastPostPublished, [
-          "name",
-          "body",
-          "summary",
-          "thumbnailURL",
-          "imageURL",
-          "imageAlt",
-          "url",
-          "publishedOn",
-          "author",
-          "category",
-          "slug",
-        ]),
-      } as BlogPostCreateInput
-
-      const imageName = `${createData.slug}-image`
-      const title = `${createData.name} Image`
-      const alt = createData.imageAlt ? createData.imageAlt : title
-
-      const imageData = await this.image.uploadImageFromURL(
-        createData.imageURL,
-        imageName,
-        imageName
-      )
-
-      const blogImage = await this.prisma.client.upsertImage({
-        where: { url: imageData.url },
-        create: { ...imageData, title, alt },
-        update: { ...imageData, title, alt },
-      })
-
       await this.prisma.client.createBlogPost({
-        ...createData,
+        ...blogData,
         image: {
           connect: { id: blogImage.id },
+        },
+      })
+    } else if (lastPostPublished.id === lastPostStored.webflowId) {
+      await this.prisma.client.updateBlogPost({
+        where: { id: lastPostStored.id },
+        data: {
+          ...blogData,
+          image: {
+            connect: { id: blogImage.id },
+          },
         },
       })
     }
