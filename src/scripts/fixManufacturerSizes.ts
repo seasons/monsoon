c94799ca99fcec87b127d27277c95f429bd0a97b
Kieran Gillen <kgillen@gmail.com>
2021-05-18 10:30:42 -0700

    Update sizeConversion JSON

--- src/scripts/fixManufacturerSizes.ts
+++ src/scripts/fixManufacturerSizes.ts
@@ -1,37 +1,64 @@
 import "module-alias/register"
 
+import { head } from "lodash"
+
+import { ProductUtilsService } from "../modules/Product/services/product.utils.service"
+import { UtilsService } from "../modules/Utils/services/utils.service"
 import { PrismaService } from "../prisma/prisma.service"
 
 const run = async () => {
   const ps = new PrismaService()
+  const utils = new UtilsService(ps)
+  const productUtils = new ProductUtilsService(ps, utils)
 
-  const sizes = await ps.client.sizes()
+  const productVariants = await ps.binding.query.productVariants(
+    {},
+    `{
+      id
+      displayShort
+      manufacturerSizes {
+        id
+        display
+        productType
+        type
+        bottom {
+          id
+          value
+          type
+        }
+        top {
+          id
+          letter
+        }
+      }
+  }`
+  )
 
-  for (const size of sizes) {
+  for (const productVariant of productVariants) {
+    const size = head(productVariant.manufacturerSizes)
     // Fix for JP sizes
-    if (size.display.includes("JP ")) {
-      const newDisplay = size.display.replace("JP ", "")
-      await ps.client.updateSize({
-        where: { id: size.id },
-        data: {
-          display: newDisplay,
-        },
-      })
-    }
-  }
-
-  const variants = await ps.client.productVariants()
-
-  for (const variant of variants) {
-    // Fix for JP sizes
-    if (variant.displayShort.includes("JP ")) {
-      const newDisplay = variant.displayShort.replace("JP ", "")
-      await ps.client.updateProductVariant({
-        where: { id: variant.id },
-        data: {
-          displayShort: newDisplay,
-        },
-      })
+    if (size.type === "EU") {
+      const display = productUtils.coerceSizeDisplayIfNeeded(
+        size.display,
+        size.type,
+        size.productType
+      )
+      if (display !== productVariant.displayShort) {
+        console.log(
+          "Updating: ",
+          productVariant.id,
+          " / Old display ",
+          productVariant.displayShort,
+          " / New display ",
+          display
+        )
+        await ps.client.updateProductVariant({
+          where: { id: productVariant.id },
+          data: {
+            displayShort: display,
+          },
+        })
+      }
     }
   }
 }
