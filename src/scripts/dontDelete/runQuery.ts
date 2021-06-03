31236d4594f34565682ba3cfa7848104c4e71c87
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-06-03 13:15:38 -0400

    add prisma generate to circleci

--- src/scripts/dontDelete/runQuery.ts
+++ src/scripts/dontDelete/runQuery.ts
@@ -4,6 +4,7 @@ import * as util from "util"
 
 import { PrismaSelect } from "@paljs/plugins"
 import { Prisma } from "@prisma/client"
+import { head } from "lodash"
 import zipcodes from "zipcodes"
 
 import { DripService } from "../../modules/Drip/services/drip.service"
@@ -22,23 +23,37 @@ const context = {
 
 const run = async () => {
   const ps = new PrismaService()
-  const info = `{
-    id
-    status
-    customer {
-      id
-    }
-    lineItems {
-        id
-        recordType
-        recordID
-    }
-  }`
-  const select = QueryUtilsService.infoToSelect(
-    info,
-    "ProductVariant",
-    context.modelFieldsByModelName
+  const a = await ps.client2.product.findMany({
+    where: {
+      brand: { shopifyShop: { enabled: true } },
+      variants: {
+        some: { shopifyProductVariant: { cacheExpiresAt: { lt: new Date() } } },
+      },
+    },
+  })
+  const b = head(
+    await ps.client2.physicalProduct.findMany({
+      where: { price: { buyUsedEnabled: true }, inventoryStatus: "Reserved" },
+      select: {
+        inventoryStatus: true,
+        productVariant: {
+          select: { product: { select: { slug: true } }, id: true },
+        },
+      },
+    })
   )
-  console.log(util.inspect(select, { depth: null }))
+  const c = await ps.client2.bagItem.findFirst({
+    where: {
+      productVariant: { id: b.productVariant[0].id },
+      status: "Reserved",
+    },
+    select: {
+      customer: { select: { user: { select: { email: true } } } },
+      productVariant: {
+        select: { product: { select: { slug: true } }, sku: true },
+      },
+    },
+  })
+  console.dir(c, { depth: null })
 }
 run()
