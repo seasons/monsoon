762544f569ce86144c9a1bef1d2c65b52d0385cd
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-06-11 13:03:19 -0400

    test txn

--- src/scripts/dontDelete/runQuery.ts
+++ src/scripts/dontDelete/runQuery.ts
@@ -5,6 +5,7 @@ import * as util from "util"
 import { PrismaSelect } from "@paljs/plugins"
 import { Prisma } from "@prisma/client"
 import { head } from "lodash"
+import cuid from 'cuid'
 import zipcodes from "zipcodes"
 
 import { DripService } from "../../modules/Drip/services/drip.service"
@@ -23,6 +24,7 @@ const context = {
 
 const run = async () => {
   const ps = new PrismaService()
+<<<<<<< Updated upstream
   const status = "Completed"
   const _latestReservation = await ps.client2.reservation.findFirst({
     where: {
@@ -52,5 +54,39 @@ const run = async () => {
     }),
   })
   console.dir(_latestReservation)
+=======
+  const txn1 = ps.client2.product.upsert({
+    where: { slug: "test-slug" },
+    create: {
+      buyNewEnabled: true,
+      slug: "test-slug",
+      name: "test-prod",
+      functions: {
+        connectOrCreate: {
+          where: { name: "yo-func" },
+          create: { name: "yo-func" },
+        },
+      },
+    },
+    update: {
+      buyNewEnabled: false,
+    },
+  })
+  const prodVarId = cuid()
+  const txn2 = ps.client2.productVariant.create({
+    data: {
+      id: prodVarId
+      sku: "test-sku-1",
+      productID: "test-slug",
+      product: { connect: { slug: "test-slug" } },
+      total: 1,
+      reservable: 0,
+      reserved: 0,
+      nonReservable: 1,
+      stored: 0,
+      offloaded: 0
+    },
+  })
+>>>>>>> Stashed changes
 }
 run()
