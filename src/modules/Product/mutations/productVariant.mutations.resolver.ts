35c0b9bde3f86c78d52b279c6f289c9af1bf35ce
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-06-14 23:24:29 -0400

    throw error on addProductVariantWant mutation

--- src/modules/Product/mutations/productVariant.mutations.resolver.ts
+++ src/modules/Product/mutations/productVariant.mutations.resolver.ts
@@ -68,31 +68,7 @@ export class ProductVariantMutationsResolver {
 
   @Mutation()
   async addProductVariantWant(@Args() { variantID }, @User() user) {
-    if (!user) throw new Error("Missing user from context")
-
-    const productVariant = await this.prisma.client.productVariant({
-      id: variantID,
-    })
-    if (!productVariant) {
-      throw new Error("Unable to find product variant with matching ID")
-    }
-
-    const productVariantWant = await this.prisma.client.createProductVariantWant(
-      {
-        isFulfilled: false,
-        productVariant: {
-          connect: {
-            id: productVariant.id,
-          },
-        },
-        user: {
-          connect: {
-            id: user.id,
-          },
-        },
-      }
-    )
-    return productVariantWant
+    throw new Error(`Deprecated. Use product restock notification instead`)
   }
 
   @Mutation()
