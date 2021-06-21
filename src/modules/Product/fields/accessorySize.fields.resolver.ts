a767ddf6adcc3bf3143c8c4b75859e2519e05616
Kieran Gillen <kgillen@gmail.com>
2021-06-21 15:29:41 -0400

    Adds size fields resolvers for accessories

--- src/modules/Product/fields/accessorySize.fields.resolver.ts
+++ src/modules/Product/fields/accessorySize.fields.resolver.ts
@@ -1,157 +1,152 @@
-import { Select } from "@app/decorators/select.decorator"
-import { PrismaService } from "@app/prisma/prisma.service"
-import { Args, Parent, ResolveField, Resolver } from "@nestjs/graphql"
+import { Loader } from "@app/modules/DataLoader/decorators/dataloader.decorator"
+import { PrismaTwoLoader } from "@app/prisma/prisma2.loader"
+import { Parent, ResolveField, Resolver } from "@nestjs/graphql"
+import { Prisma } from "@prisma/client"
 
-import { ProductService } from "../services/product.service"
 import { ProductUtilsService } from "../services/product.utils.service"
 
 @Resolver("AccessorySize")
 export class AccessorySizeFieldsResolver {
-  constructor(
-    private readonly prisma: PrismaService,
-    private readonly productService: ProductService,
-    private readonly productUtilsService: ProductUtilsService
-  ) {}
+  constructor(private readonly productUtilsService: ProductUtilsService) {}
 
   @ResolveField()
-  async bridge(@Parent() parent) {
-    const accessorySize = await this.prisma.client2.accessorySize.findUnique({
-      where: { id: parent.id },
-      select: {
-        size: {
-          select: {
-            productVariantsInternal: {
-              select: {
-                productID: true,
+  async bridge(
+    @Parent() parent,
+    @Loader({
+      type: PrismaTwoLoader.name,
+      params: {
+        model: "AccessorySize",
+        select: Prisma.validator<Prisma.AccessorySizeSelect>()({
+          id: true,
+          size: {
+            select: {
+              productVariantsInternal: {
+                select: {
+                  product: {
+                    select: {
+                      category: {
+                        select: {
+                          measurementType: true,
+                        },
+                      },
+                    },
+                  },
+                },
               },
             },
           },
-        },
+        }),
       },
     })
-    const productID = accessorySize?.size?.productVariantsInternal?.productID
-    if (productID) {
-      const product = await this.prisma.client2.product.findUnique({
-        where: { slug: productID },
-        select: {
-          category: {
-            select: {
-              measurementType: true,
-            },
-          },
-        },
-      })
-
+    accessorySizeLoader
+  ) {
+    const accessorySize = await accessorySizeLoader.load(parent.id)
+    const measurementType =
+      accessorySize?.size?.productVariantsInternal?.product?.category
+        ?.measurementType
+    if (measurementType && parent.bridge) {
       return Math.round(
         this.productUtilsService.convertInchesToMeasurementSize(
           parent.bridge,
-          product?.category?.measurementType
+          measurementType
         )
       )
+    } else {
+      return undefined
     }
   }
 
   @ResolveField()
-  async length(@Parent() parent) {
-    const accessorySize = await this.prisma.client2.accessorySize.findUnique({
-      where: { id: parent.id },
-      select: {
-        size: {
-          select: {
-            productVariantsInternal: {
-              select: {
-                productID: true,
+  async length(
+    @Parent() parent,
+    @Loader({
+      type: PrismaTwoLoader.name,
+      params: {
+        model: "AccessorySize",
+        select: Prisma.validator<Prisma.AccessorySizeSelect>()({
+          id: true,
+          size: {
+            select: {
+              productVariantsInternal: {
+                select: {
+                  product: {
+                    select: {
+                      category: {
+                        select: {
+                          measurementType: true,
+                        },
+                      },
+                    },
+                  },
+                },
               },
             },
           },
-        },
+        }),
       },
     })
-    const productID = accessorySize?.size?.productVariantsInternal?.productID
-    if (productID) {
-      const product = await this.prisma.client2.product.findUnique({
-        where: { slug: productID },
-        select: {
-          category: {
-            select: {
-              measurementType: true,
-            },
-          },
-        },
-      })
-
+    accessorySizeLoader
+  ) {
+    const accessorySize = await accessorySizeLoader.load(parent.id)
+    const measurementType =
+      accessorySize?.size?.productVariantsInternal?.product?.category
+        ?.measurementType
+    if (measurementType && parent.length) {
       return Math.round(
         this.productUtilsService.convertInchesToMeasurementSize(
           parent.length,
-          product?.category?.measurementType
+          measurementType
         )
       )
+    } else {
+      return undefined
     }
   }
 
   @ResolveField()
-  async width(@Parent() parent) {
-    const accessorySize = await this.prisma.client2.accessorySize.findUnique({
-      where: { id: parent.id },
-      select: {
-        size: {
-          select: {
-            productVariantsInternal: {
-              select: {
-                productID: true,
+  async width(
+    @Parent() parent,
+    @Loader({
+      type: PrismaTwoLoader.name,
+      params: {
+        model: "AccessorySize",
+        select: Prisma.validator<Prisma.AccessorySizeSelect>()({
+          id: true,
+          size: {
+            select: {
+              productVariantsInternal: {
+                select: {
+                  product: {
+                    select: {
+                      category: {
+                        select: {
+                          measurementType: true,
+                        },
+                      },
+                    },
+                  },
+                },
               },
             },
           },
-        },
+        }),
       },
     })
-    const productID = accessorySize?.size?.productVariantsInternal?.productID
-    if (productID) {
-      const product = await this.prisma.client2.product.findUnique({
-        where: { slug: productID },
-        select: {
-          category: {
-            select: {
-              measurementType: true,
-            },
-          },
-        },
-      })
-
+    accessorySizeLoader
+  ) {
+    const accessorySize = await accessorySizeLoader.load(parent.id)
+    const measurementType =
+      accessorySize?.size?.productVariantsInternal?.product?.category
+        ?.measurementType
+    if (measurementType && parent.width) {
       return Math.round(
         this.productUtilsService.convertInchesToMeasurementSize(
           parent.width,
-          product?.category?.measurementType
+          measurementType
         )
       )
+    } else {
+      return undefined
     }
-
-    return undefined
   }
-
-  //   @ResolveField()
-  //   async modelHeight(
-  //     @Parent() parent,
-  //     @Loader({
-  //       type: PrismaTwoLoader.name,
-  //       params: {
-  //         model: "Product",
-  //         select: Prisma.validator<Prisma.ProductSelect>()({
-  //           id: true,
-  //           model: { select: { id: true, height: true } },
-  //         }),
-  //       },
-  //     })
-  //     productLoader: PrismaTwoDataLoader<{
-  //       id: string
-  //       model: Pick<ProductModel, "id" | "height">
-  //     }>
-  //   ) {
-  //     let product = parent
-  //     if (!parent.model?.height) {
-  //       product = await productLoader.load(product.id)
-  //     }
-
-  //     return product.model?.height
-  //   }
 }
