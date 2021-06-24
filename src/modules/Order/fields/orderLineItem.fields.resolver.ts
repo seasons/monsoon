4392f3975618d4df20d85b87ca461e8c94ebdb40
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-06-24 13:37:30 -0400

    order line item field resolvers to prisma two

--- src/modules/Order/fields/orderLineItem.fields.resolver.ts
+++ src/modules/Order/fields/orderLineItem.fields.resolver.ts
@@ -1,42 +1,47 @@
-import { PrismaService } from "@app/prisma/prisma.service"
-import { Info, Parent, ResolveField, Resolver } from "@nestjs/graphql"
+import { Loader } from "@app/modules/DataLoader/decorators/dataloader.decorator"
+import { PrismaTwoLoader } from "@app/prisma/prisma2.loader"
+import { Parent, ResolveField, Resolver } from "@nestjs/graphql"
+import { Prisma } from "@prisma/client"
 
 @Resolver("OrderLineItem")
 export class OrderLineItemFieldsResolver {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor() {}
 
   @ResolveField()
-  async productVariant(@Parent() orderItem, @Info() info) {
+  async productVariant(
+    @Parent() orderItem,
+    @Loader({
+      type: PrismaTwoLoader.name,
+      params: {
+        model: "ProductVariant",
+        formatWhere: keys =>
+          Prisma.validator<Prisma.ProductVariantWhereInput>()({
+            physicalProducts: { some: { id: { in: keys } } },
+          }),
+      },
+      includeInfo: true,
+    })
+    productVariantFromPhysicalProductLoader,
+    @Loader({
+      type: PrismaTwoLoader.name,
+      params: {
+        model: "ProductVariant",
+      },
+      includeInfo: true,
+    })
+    productVariantFromIdLoader
+  ) {
     const recordType = orderItem.recordType
 
     if (recordType === "PhysicalProduct") {
-      const productVariantID = await this.prisma.client
-        .physicalProduct({
-          id: orderItem.recordID,
-        })
-        .productVariant()
-        .id()
-
-      return await this.prisma.binding.query.productVariant(
-        {
-          where: {
-            id: productVariantID,
-          },
-        },
-        info
+      return await productVariantFromPhysicalProductLoader.load(
+        orderItem.recordID
       )
     } else if (
       recordType === "ProductVariant" ||
       recordType === "ExternalProduct"
     ) {
-      return await this.prisma.binding.query.productVariant(
-        {
-          where: {
-            id: orderItem.recordID,
-          },
-        },
-        info
-      )
+      return await productVariantFromIdLoader.load(orderItem.recordID)
     } else {
       return null
     }
