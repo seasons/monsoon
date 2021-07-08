d2a0ce71d7f1810aa842a1c1630053d8261d4458
Luc Succes <l2succes@gmail.com>
2021-07-08 14:08:49 -0500

    Remove ShopifyController

--- src/modules/Shopify/shopify.module.ts
+++ src/modules/Shopify/shopify.module.ts
@@ -1,13 +1,11 @@
 import { PrismaModule } from "@app/prisma/prisma.module"
 import { Module } from "@nestjs/common"
 
-import { ShopifyController } from "./controllers/shopify.controller"
 import { ShopifyMutationsResolver } from "./mutations/shopify.mutations"
 import { ShopifyQueriesResolver } from "./queries/shopify.queries.resolver"
 import { ShopifyService } from "./services/shopify.service"
 
 @Module({
-  controllers: [ShopifyController],
   imports: [PrismaModule],
   providers: [ShopifyService, ShopifyQueriesResolver, ShopifyMutationsResolver],
   exports: [ShopifyService],
