5f2c53a604c44842400b46f02b9d075402243a1b
Dmitry Pavluk <dmitry@canopyservicing.com>
2021-07-20 15:01:32 -0400

    pipe logs directly to DD via transport, set up traces, user context

--- src/tracer.ts
+++ src/tracer.ts
@@ -2,16 +2,7 @@ import tracer from "dd-trace"
 
 tracer.init({
   logInjection: true,
-  runtimeMetrics: true,
-})
-
-// enable and configure postgresql integration
-tracer.use("pg", {
-  service: `monsoon-${process.env.NODE_ENV}`,
-})
-
-tracer.use("graphql", {
-  service: `monsoon-${process.env.NODE_ENV}`,
+  runtimeMetrics: process.env.NODE_ENV === "production",
 })
 
 export default tracer
