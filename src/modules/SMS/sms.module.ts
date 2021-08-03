110c26b426593fce357c6a776f92716716890efa
Kieran Gillen <kgillen@gmail.com>
2021-08-03 11:39:38 -0400

    Removes unused ref

--- src/modules/SMS/sms.module.ts
+++ src/modules/SMS/sms.module.ts
@@ -1,4 +1,4 @@
-import { Module, forwardRef } from "@nestjs/common"
+import { Module } from "@nestjs/common"
 import { PrismaModule } from "@prisma1/prisma.module"
 
 import { EmailModule } from "../Email/email.module"
