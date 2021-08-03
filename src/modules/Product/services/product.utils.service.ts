3260234d8127c88b1dcd3c93840fc98d3e671fb0
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-08-03 16:27:25 -0400

    take in skip on products conneciton query

--- src/modules/Product/services/product.utils.service.ts
+++ src/modules/Product/services/product.utils.service.ts
@@ -126,7 +126,7 @@ export class ProductUtilsService {
           ...where,
           ...filters?.where,
         },
-        ...pick(args, ["first", "last", "after", "before"]),
+        ...pick(args, ["first", "last", "after", "before", "skip"]),
       },
       identity
     )
