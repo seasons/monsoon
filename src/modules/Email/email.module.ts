f48e08cdb8f0bc9524c07232d5e01e490e3bbad5
Kieran Gillen <kgillen@gmail.com>
2021-08-02 20:50:20 -0400

    Removes import

--- src/modules/Email/email.module.ts
+++ src/modules/Email/email.module.ts
@@ -3,19 +3,12 @@ import { Module } from "@nestjs/common"
 import { PrismaModule } from "../../prisma/prisma.module"
 import { ErrorModule } from "../Error/error.module"
 import { ImageModule } from "../Image/image.module"
-import { ReservationModule } from "../Reservation/reservation.module"
 import { UtilsModule } from "../Utils/utils.module"
 import { EmailService } from "./services/email.service"
 import { EmailUtilsService } from "./services/email.utils.service"
 
 @Module({
-  imports: [
-    PrismaModule,
-    UtilsModule,
-    ErrorModule,
-    ImageModule,
-    ReservationModule,
-  ],
+  imports: [PrismaModule, UtilsModule, ErrorModule, ImageModule],
   providers: [EmailService, EmailUtilsService],
   exports: [EmailService],
 })
