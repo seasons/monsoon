cd1fd426e3a46e70e3a82da4fba873f3fc1edb68
Luc Succes <l2succes@gmail.com>
2021-08-18 15:38:23 -0400

    Adds multiple column constraints

---
+++ prisma/migrations/20210817210520_multiple_column_constraints/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `lostAt` on the `Reservation` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[productVariant,user]` on the table `ProductVariantWant` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[reservation,user]` on the table `ReservationFeedback` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[reservation]` on the table `ReservationFeedback` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[shippingMethod]` on the table `ShippingOption` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[top]` on the table `Size` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "Reservation" DROP COLUMN "lostAt";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProductVariantWant.productVariant_user_unique" ON "ProductVariantWant"("productVariant", "user");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ReservationFeedback.reservation_user_unique" ON "ReservationFeedback"("reservation", "user");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ReservationFeedback_reservation_unique" ON "ReservationFeedback"("reservation");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ShippingOption_shippingMethod_unique" ON "ShippingOption"("shippingMethod");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Size_top_unique" ON "Size"("top");
