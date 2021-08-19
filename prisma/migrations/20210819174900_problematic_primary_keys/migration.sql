310b6d6c24aea1c873c0f223259df3d470180e05
Luc Succes <l2succes@gmail.com>
2021-08-19 14:32:09 -0400

    Update schema.prisma and migrations

--- prisma/migrations/20210819174900_problematic_primary_keys/migration.sql
+++ prisma/migrations/20210819174900_problematic_primary_keys/migration.sql
@@ -3,9 +3,8 @@
 
   - You are about to drop the column `lostAt` on the `Reservation` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[productVariant,user]` on the table `ProductVariantWant` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[reservation,user]` on the table `ReservationFeedback` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[reservation]` on the table `ReservationFeedback` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[shippingMethod]` on the table `ShippingOption` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[origin,destination,shippingMethod]` on the table `ShippingOption` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[top]` on the table `Size` will be added. If there are existing duplicate values, this will fail.
 
 */
@@ -16,13 +15,10 @@ ALTER TABLE "Reservation" DROP COLUMN "lostAt";
 CREATE UNIQUE INDEX "ProductVariantWant.productVariant_user_unique" ON "ProductVariantWant"("productVariant", "user");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "ReservationFeedback.reservation_user_unique" ON "ReservationFeedback"("reservation", "user");
-
--- CreateIndex
 CREATE UNIQUE INDEX "ReservationFeedback_reservation_unique" ON "ReservationFeedback"("reservation");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "ShippingOption_shippingMethod_unique" ON "ShippingOption"("shippingMethod");
+CREATE UNIQUE INDEX "ShippingOption.origin_destination_shippingMethod_unique" ON "ShippingOption"("origin", "destination", "shippingMethod");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Size_top_unique" ON "Size"("top");
