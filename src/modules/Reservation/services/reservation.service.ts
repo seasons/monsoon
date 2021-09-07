d0ecdce1b40a223a9a6e34e5c16790173deb8868
Luc Succes <l2succes@gmail.com>
2021-09-07 17:08:22 -0400

    Only include new items in reservation confirmation screen

--- src/modules/Reservation/services/reservation.service.ts
+++ src/modules/Reservation/services/reservation.service.ts
@@ -659,6 +659,7 @@ export class ReservationService {
             id: customer.id,
           },
           saved: false,
+          status: "Added",
         },
         select: {
           id: true,
