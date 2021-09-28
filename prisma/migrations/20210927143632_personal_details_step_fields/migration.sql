35f260aecdbafa6130bb4d65280addbe1c83cf37
Kieran Gillen <kgillen@gmail.com>
2021-09-28 16:21:50 +0100

    Updates migration

--- prisma/migrations/20210927143632_personal_details_step_fields/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "CustomerDetail" ADD COLUMN     "ageRange" TEXT,
-ADD COLUMN     "signupReasons" TEXT[];
-
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "customerDetail" VARCHAR(30);
-
--- AddForeignKey
-ALTER TABLE "Product" ADD FOREIGN KEY ("customerDetail") REFERENCES "CustomerDetail"("id") ON DELETE SET NULL ON UPDATE CASCADE;
