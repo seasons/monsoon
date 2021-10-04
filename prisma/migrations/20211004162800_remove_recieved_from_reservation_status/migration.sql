8190da0c98bfc93438a2b8593cd124fd822dbbf3
Oliver <reviloz223@gmail.com>
2021-10-04 12:34:44 -0400

    remove received from reservation status

---
+++ prisma/migrations/20211004162800_remove_recieved_from_reservation_status/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [Received] on the enum `ReservationStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "ReservationStatus_new" AS ENUM ('Queued', 'Picked', 'Packed', 'Shipped', 'Delivered', 'Completed', 'Cancelled', 'Hold', 'Blocked', 'Unknown', 'Lost', 'EarlyReturned', 'ReturnPending');
+ALTER TABLE "Reservation" ALTER COLUMN "status" TYPE "ReservationStatus_new" USING ("status"::text::"ReservationStatus_new");
+ALTER TYPE "ReservationStatus" RENAME TO "ReservationStatus_old";
+ALTER TYPE "ReservationStatus_new" RENAME TO "ReservationStatus";
+DROP TYPE "ReservationStatus_old";
+COMMIT;
