4a754521835d8b762d2ccad832a3c474d3f57000
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-10-10 01:47:22 -0400

    fix segment event

--- src/modules/Analytics/controllers/segment.controller.ts
+++ src/modules/Analytics/controllers/segment.controller.ts
@@ -18,14 +18,8 @@ export class SegmentController {
   @Post()
   async handlePost(@Body() body: SegmentEvent) {
     if (body.type === "identify" && body.userId !== null) {
-      const versionString = body.context.app.version
-      const [major, minor, patch] = versionString.split(".")
-
       const data = {
-        iOSVersion: versionString,
-        iOSMajorVersion: Number(major),
-        iOSMinorVersion: Number(minor),
-        iOSPatch: patch ? Number(patch) : 0,
+        iOSVersion: body.context.app.version,
       }
       await this.prisma.client.user.update({
         where: { id: body.userId },
