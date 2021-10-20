4935352201796769a7b34676e53ee5c642d94b17
Luc Succes <l2succes@gmail.com>
2021-10-20 03:40:37 -0400

    Take line item fields into account

--- src/modules/Payment/services/rental.service.ts
+++ src/modules/Payment/services/rental.service.ts
@@ -686,7 +686,11 @@ export class RentalService {
           ...daysRentedMetadata
         } = await this.calcDaysRented(invoice, physicalProduct)
 
-        const price = await this.calculatePriceForDaysRented({
+        const {
+          price,
+          adjustedForPreviousMinimum,
+          appliedMinimum,
+        } = await this.calculatePriceForDaysRented({
           invoice,
           customer: custWithExtraData,
           product: physicalProduct,
@@ -698,6 +702,8 @@ export class RentalService {
           daysRented,
           physicalProduct: { connect: { id: physicalProduct.id } },
           price,
+          appliedMinimum,
+          adjustedForPreviousMinimum,
         }) as Prisma.RentalInvoiceLineItemCreateInput
       })
     )) as any
