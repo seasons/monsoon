e10a7fd566047f2d3cbba55c5ef091383fb1c5dc
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-10-20 21:43:30 -0400

    add email service mock file

---
+++ src/modules/Utils/tests/emailService.mock.ts
@@ -0,0 +1,19 @@
+// A generic email service mock that wont send any emails in a test suite
+export class EmailServiceMock {
+  sendSubmittedEmailEmail = async () => null
+  sendAuthorizedEmail = async () => null
+  sendRecommendedItemsNurtureEmail = async () => null
+  sendAuthorizedDayTwoFollowup = async () => null
+  sendBuyUsedOrderConfirmationEmail = async () => null
+  sendUnpaidMembershipEmail = async => () => null
+  sendReturnToGoodStandingEmail = async () => null
+  sendReferralConfirmationEmail = async () => null
+  sendWaitlistedEmail = async () => null
+  sendSubscribedEmail = async () => null
+  sendAdminConfirmationEmail = async () => null
+  sendPriorityAccessEmail = async () => null
+  sendReservationConfirmationEmail = async () => null
+  sendReturnReminderEmail = async () => null
+  sendRestockNotificationEmails = async () => null
+  sendYouCanNowReserveAgainEmail = async () => null
+}
