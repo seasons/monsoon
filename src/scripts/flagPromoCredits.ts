e15efc33e57cbbb6e31412a2a9ae66e30601d703
Oliver <reviloz223@gmail.com>
2021-10-27 17:55:26 -0400

    reconcile Micah Clark

--- src/scripts/flagPromoCredits.ts
+++ src/scripts/flagPromoCredits.ts
@@ -556,6 +556,7 @@ const printFlags = async (
 ) => {
   const uniqueFlags = uniqBy(flags, a => a.id)
   const reconciled = [
+    "ckp8arasj18za0500fhho7hyd", // Micah Clark
     "cko0o08xr25zl0773au9siyv6", // Perico Arcedo
     "ckp8pxdyw0il80505u50uoohz", // Raquan Cotton
     "ckhxybiij04m30737ubd8kwmb", // Jesse Samburge
