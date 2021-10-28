c7383dacf2cc8e23f19dbb6788122793478a5438
Oliver <reviloz223@gmail.com>
2021-10-28 16:06:37 -0400

    reconcile Jaddar Ford

--- src/scripts/flagPromoCredits.ts
+++ src/scripts/flagPromoCredits.ts
@@ -596,6 +596,7 @@ const printFlags = async (
 ) => {
   const uniqueFlags = uniqBy(flags, a => a.id)
   const reconciled = [
+    "ckhjdhq2m0h1g0799zvr5fxco", // Jaddar Ford
     "ckl1nibbt0p8f0717v2ej8x11", // Jonathan O'Rorke
     "ckkj6bltn0sz60739q29geoso", // Doiminique McGhee
     "cksxrlcvo5712202f2kqdjognpy", // Nicholas Hatcher
