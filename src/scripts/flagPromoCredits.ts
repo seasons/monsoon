36d7ff83f81850c9bc0ae09a5f1b4039296f30bd
Oliver <reviloz223@gmail.com>
2021-10-28 10:12:19 -0400

    reoncile Chad Frisbie

--- src/scripts/flagPromoCredits.ts
+++ src/scripts/flagPromoCredits.ts
@@ -596,6 +596,7 @@ const printFlags = async (
 ) => {
   const uniqueFlags = uniqBy(flags, a => a.id)
   const reconciled = [
+    "cksrt1tpf14808242fty8sb3kqxu", // Chad Frisbie
     "ckfyi7tdy078e0781ffql9z75", // Diego Lopez
     "ckcp88wnp1ipt0740piinrz3c", // Maria Montgomery
     "ck2zc5u8u0nub07341d18sok7", // Luke Beard
