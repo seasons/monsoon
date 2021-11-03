186b21cd989cf038276a0ed24ee4f5dce8031b0b
Oliver <reviloz223@gmail.com>
2021-11-03 17:16:51 -0400

    audit the rental invoices that have a gap

--- src/scripts/dontDelete/runQuery.ts
+++ src/scripts/dontDelete/runQuery.ts
@@ -5,15 +5,83 @@ import { PrismaService } from "../../prisma/prisma.service"
 const run = async () => {
   const ps = new PrismaService()
 
-  const invoiceId = "cktt8xz7s9298q6uvwyrsnrut"
-  const x = await ps.client.rentalInvoice.findUnique({
-    where: { id: invoiceId },
+  // const invoiceId = "cktt8xz7s9298q6uvwyrsnrut"
+  // const x = await ps.client.rentalInvoice.findUnique({
+  //   where: { id: invoiceId },
+  //   select: {
+  //     status: true,
+  //     lineItems: { select: { price: true, name: true, comment: true } },
+  //     membership: { select: { customer: { select: { user: true } } } },
+  //   },
+  // })
+  // console.dir(x, { depth: null })
+
+  const customersWithRentalInvoice = await ps.client.customer.findMany({
+    where: {
+      membership: {
+        rentalInvoices: {
+          some: {
+            status: "Draft",
+          },
+        },
+      },
+      user: {
+        NOT: {
+          email: {
+            contains: "@seasons.nyc",
+          },
+        },
+      },
+    },
     select: {
-      status: true,
-      lineItems: { select: { price: true, name: true, comment: true } },
-      membership: { select: { customer: { select: { user: true } } } },
+      user: {
+        select: {
+          email: true,
+        },
+      },
+      membership: {
+        select: {
+          rentalInvoices: {
+            orderBy: {
+              billingStartAt: "asc",
+            },
+            select: {
+              billingEndAt: true,
+              billingStartAt: true,
+            },
+          },
+        },
+      },
     },
   })
-  console.dir(x, { depth: null })
+
+  const filteredCustomers = customersWithRentalInvoice.filter(
+    x => x.membership?.rentalInvoices.length > 1
+  )
+
+  const customers = []
+  filteredCustomers.forEach(customer => {
+    const rentalInvoices = [...customer.membership.rentalInvoices]
+
+    let previousRentalInvoice = rentalInvoices.shift()
+
+    while (rentalInvoices.length > 0) {
+      const currentRentalInvoice = rentalInvoices.shift()
+      const previousRentalInvoiceDate = previousRentalInvoice.billingEndAt.getDate()
+      const currentRentalInvoiceDate = currentRentalInvoice.billingStartAt.getDate()
+
+      const dateDiff = Math.abs(
+        previousRentalInvoiceDate - currentRentalInvoiceDate
+      )
+
+      if (dateDiff > 3) {
+        console.dir(customer, { depth: null })
+        customers.push(customer)
+        return
+      }
+      previousRentalInvoice = currentRentalInvoice
+    }
+  })
+  console.log(customers.length)
 }
 run()
