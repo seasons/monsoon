362345e294c983774e3b7c8dc48c1b23451e75ef
Oliver <reviloz223@gmail.com>
2021-11-06 21:04:57 -0400

    add comments to markItemAsLost spec file

--- src/modules/Product/tests/markItemsAsLost.spec.ts
+++ src/modules/Product/tests/markItemsAsLost.spec.ts
@@ -6,11 +6,80 @@ import { PrismaService } from "@app/prisma/prisma.service"
 import { Test } from "@nestjs/testing"
 import { InventoryStatus } from "@prisma/client"
 
+import { ProductModuleDef } from "../product.module"
+import { BagService } from "../services/bag.service"
+import { PhysicalProductService } from "../services/physicalProduct.service"
+
 describe("Mark items as lost", () => {
   let prismaService: PrismaService
-  let productService: ProductUtilsService
-  /**
-   *
-   *
-   */
+  let bagService: BagService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule(ProductModuleDef).compile()
+    prismaService = moduleRef.get<PrismaService>(PrismaService)
+    bagService = moduleRef.get<BagService>(BagService)
+  })
+
+  it("removes bag items from customer's bag", () => {
+    /**
+     * create customer
+     * place reservation(add items to bag, make reservation)
+     * ship reservation to customer
+     * mark items as lost
+     * check to see if items were removed from customer's bag
+     */
+  })
+
+  it("sets the lostAt, isLost for relevant reservationPhysicalProducts", () => {
+    /**
+     * create reservation
+     * place reservation
+     * ship reservation
+     * mark items as lost
+     * check to see if reservationPhysicalProduct's lostAt time stamp has been set
+     * check to see if reservationPhysicalProduct's isLost boolean has been set
+     *
+     */
+  })
+
+  it("sets lostInPhase for outbound ppackage as BusinessToCustomer", () => {
+    /**
+     * create reservation
+     * place reservation
+     * ship reservation to customer
+     * mark items as lost
+     * check to see that lostInPhase is set to BusinessToCustomer
+     */
+  })
+
+  it("sets lostInPhase for inbound package to CustomerToBusiness", () => {
+    /**
+     * create reservation
+     * place reservation
+     * ship reservation to customer
+     * ship reservation back to business
+     * mark items as lost
+     * check to see that lostInPhase is set to CustomerToBusiness
+     */
+  })
+
+  it("updates product variant counts", () => {
+    /**
+     * create reservation
+     * place reservation
+     * ship reservation
+     * mark items as lost
+     * check product variant counts
+     */
+  })
+
+  it("sets physicalProduct inventory status to NonReservable and product status to lost", () => {
+    /**
+     * create reseravtion
+     * place reservation
+     * ship reservation
+     * mark items as lost
+     * check physicalProduct inventory status
+     */
+  })
 })
