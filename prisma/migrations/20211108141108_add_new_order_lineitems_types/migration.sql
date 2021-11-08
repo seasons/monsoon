861ec85e172756ba778ad0aebfff67fc8273d42b
Kieran Gillen <kgillen@gmail.com>
2021-11-08 15:11:22 +0100

    Adds migration file

---
+++ prisma/migrations/20211108141108_add_new_order_lineitems_types/migration.sql
@@ -0,0 +1,13 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "OrderLineItemRecordType" ADD VALUE 'Credit';
+ALTER TYPE "OrderLineItemRecordType" ADD VALUE 'MembershipDiscount';
+
+-- AlterTable
+ALTER TABLE "CustomerMembership" ADD COLUMN     "membershipDiscountCredits" INTEGER DEFAULT 0;
