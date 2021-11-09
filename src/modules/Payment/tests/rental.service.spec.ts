8872e9b2e47fd9795b1622b29778af701c66524b
Oliver <reviloz223@gmail.com>
2021-11-09 14:08:48 -0500

    complete returnMultiItem tests

--- src/modules/Payment/tests/rental.service.spec.ts
+++ src/modules/Payment/tests/rental.service.spec.ts
@@ -275,6 +275,9 @@ describe("Rental Service", () => {
     reservationService = moduleRef.get<ReservationService>(ReservationService)
     testUtils = moduleRef.get<TestUtilsService>(TestUtilsService)
     productUtils = moduleRef.get<ProductUtilsService>(ProductUtilsService)
+    resPhysProdService = moduleRef.get<ReservationPhysicalProductService>(
+      ReservationPhysicalProductService
+    )
 
     const notificationService = moduleRef.get<PushNotificationService>(
       PushNotificationService
@@ -2589,17 +2592,67 @@ describe("Rental Service", () => {
   })
 
   describe("Return Items", () => {
-    it("removes items from customer's bag", () => {
+    let reservation
+    let physicalProducts
+    beforeEach(async () => {
+      const { cleanupFunc, customer } = await testUtils.createTestCustomer({
+        select: testCustomerSelect,
+      })
+      cleanupFuncs.push(cleanupFunc)
+      testCustomer = customer
+      reservation = await addToBagAndReserveForCustomer(3)
+      await setReservationStatus(reservation.id, "Delivered")
+      physicalProducts = await prisma.client.physicalProduct.findMany({
+        where: {
+          reservationPhysicalProduct: {
+            some: {
+              reservationId: reservation.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      })
+    })
+
+    it("removes items from customer's bag", async () => {
       /**
        * create customer
        * make reservation
        * set reservation to delivered
        * return items
-       * check to see if bagItems are null
+       * check to see if bagItems have been deleted
        */
+      const productStates = []
+
+      for (let physicalProduct of physicalProducts) {
+        productStates.push({
+          productUID: physicalProduct.seasonsUID,
+          returned: true,
+          productStatus: "Dirty",
+          notes: "no notes needed here",
+        })
+      }
+      await resPhysProdService.returnMultiItems({
+        productStates,
+        droppedOffBy: "UPS",
+        trackingNumber: reservation.returnPackage.trackingNumber,
+      })
+
+      const bagItems = await prisma.client.bagItem.findMany({
+        where: {
+          physicalProduct: {
+            id: {
+              in: physicalProducts.map(a => a.id),
+            },
+          },
+        },
+      })
+      expect(bagItems).toBeNull
     })
 
-    it("sets productVariant counts", () => {
+    it("sets productVariant counts", async () => {
       /**
        * create customer
        * make reservation
@@ -2607,9 +2660,45 @@ describe("Rental Service", () => {
        * return items
        * check product variant counts
        */
+
+      const productStates = []
+
+      for (let physicalProduct of physicalProducts) {
+        productStates.push({
+          productUID: physicalProduct.seasonsUID,
+          returned: true,
+          productStatus: "Dirty",
+          notes: "no notes needed here",
+        })
+      }
+      await resPhysProdService.returnMultiItems({
+        productStates,
+        droppedOffBy: "UPS",
+        trackingNumber: reservation.returnPackage.trackingNumber,
+      })
+      const productVariants = await prisma.client.productVariant.findMany({
+        where: {
+          physicalProducts: {
+            some: {
+              id: {
+                in: physicalProducts.map(a => a.id),
+              },
+            },
+          },
+        },
+        select: {
+          nonReservable: true,
+        },
+      })
+
+      for (const productVariant of productVariants) {
+        expect(productVariant.nonReservable).toBe(
+          productVariant.nonReservable - 1
+        )
+      }
     })
 
-    it("sets reservation as complete if all reservation physical products are returned", () => {
+    it("sets reservation as complete if all reservation physical products are returned", async () => {
       /**
        * create customer
        * make reservation
@@ -2617,9 +2706,35 @@ describe("Rental Service", () => {
        * return all items on the reservation
        * check to see if reservation is completed
        */
+
+      const productStates = []
+
+      for (let physicalProduct of physicalProducts) {
+        productStates.push({
+          productUID: physicalProduct.seasonsUID,
+          returned: true,
+          productStatus: "Dirty",
+          notes: "no notes needed here",
+        })
+      }
+      await resPhysProdService.returnMultiItems({
+        productStates,
+        droppedOffBy: "UPS",
+        trackingNumber: reservation.returnPackage.trackingNumber,
+      })
+      const updatedReservation = await prisma.client.reservation.findUnique({
+        where: {
+          id: reservation.id,
+        },
+        select: {
+          status: true,
+        },
+      })
+
+      expect(updatedReservation.status).toBe("Completed")
     })
 
-    it("doesn't set reservation to completed if all reservation physical products items are not returned", () => [
+    it("doesn't set reservation to completed if all reservation physical products items are not returned", async () => {
       /**
        * create customer
        * make reservation
@@ -2627,9 +2742,32 @@ describe("Rental Service", () => {
        * return only one item on the reservation
        * check to see if reservation is still in status delivered
        */
-    ])
 
-    it("sets droppedOffBy to 'UPS' and has relevant inbound package if dropped of by ups on reservationPhysicalProducts", () => {
+      const productStates = [
+        {
+          productUID: physicalProducts[0].seasonsUID,
+        },
+      ]
+
+      await resPhysProdService.returnMultiItems({
+        productStates,
+        droppedOffBy: "UPS",
+        trackingNumber: reservation.returnPackage.trackingNumber,
+      })
+
+      const updatedReservation = await prisma.client.reservation.findUnique({
+        where: {
+          id: reservation.id,
+        },
+        select: {
+          status: true,
+        },
+      })
+
+      expect(updatedReservation.status).toBe("Delivered")
+    })
+
+    it("sets droppedOffBy to 'UPS', droppedOffAt and has relevant inbound package if dropped of by ups on reservationPhysicalProducts", async () => {
       /**
        * create customer
        * make reservation
@@ -2637,9 +2775,43 @@ describe("Rental Service", () => {
        * query reservationPhysicalProducts related to reservation, filter them by hasBeenProcessed
        * check droppedOffAt and inboundPackage on filtered resPhysProds
        */
+      const productStates = []
+
+      for (let physicalProduct of physicalProducts) {
+        productStates.push({
+          productUID: physicalProduct.seasonsUID,
+          returned: true,
+          productStatus: "Dirty",
+          notes: "no notes needed here",
+        })
+      }
+      await resPhysProdService.returnMultiItems({
+        productStates,
+        droppedOffBy: "UPS",
+        trackingNumber: reservation.returnPackage.trackingNumber,
+      })
+
+      const reservationPhysProds = await prisma.client.reservationPhysicalProduct.findMany(
+        {
+          where: {
+            reservationId: reservation.id,
+          },
+          select: {
+            droppedOffAt: true,
+            droppedOffBy: true,
+            inboundPackageId: true,
+          },
+        }
+      )
+
+      for (const reservationPhysProd of reservationPhysProds) {
+        expect(reservationPhysProd.droppedOffBy).toBe("UPS")
+        expect(!!reservationPhysProd.droppedOffAt).toBe(true)
+        expect(!!reservationPhysProd.inboundPackageId).toBe(true)
+      }
     })
 
-    it("sets droppedOffBy to 'Customer' and has no package if dropped off by customer on reservationPhysicalProducts", () => {
+    it("sets droppedOffBy to 'Customer' and has no package if dropped off by customer on reservationPhysicalProducts", async () => {
       /**
        * create customer
        * make reservation
@@ -2648,9 +2820,43 @@ describe("Rental Service", () => {
        * check droppedOffAt on filtered resPhysProds
        * check to see if there is no package on filtered resPhysProd
        */
+
+      const productStates = []
+
+      for (let physicalProduct of physicalProducts) {
+        productStates.push({
+          productUID: physicalProduct.seasonsUID,
+          returned: true,
+          productStatus: "Dirty",
+          notes: "no notes needed here",
+        })
+      }
+      await resPhysProdService.returnMultiItems({
+        productStates,
+        droppedOffBy: "Customer",
+      })
+
+      const reservationPhysProds = await prisma.client.reservationPhysicalProduct.findMany(
+        {
+          where: {
+            reservationId: reservation.id,
+          },
+          select: {
+            droppedOffAt: true,
+            droppedOffBy: true,
+            inboundPackageId: true,
+          },
+        }
+      )
+
+      for (const reservationPhysProd of reservationPhysProds) {
+        expect(reservationPhysProd.droppedOffBy).toBe("Customer")
+        expect(!!reservationPhysProd.droppedOffAt).toBe(true)
+        expect(reservationPhysProd.inboundPackageId).toBeNull
+      }
     })
 
-    it("sets hasReturnProcessed and returnProcessAt on reservationPhysicalProducts", () => {
+    it("sets hasReturnProcessed and returnProcessAt on reservationPhysicalProducts", async () => {
       /**
        * create customer
        * make reservation
@@ -2659,9 +2865,39 @@ describe("Rental Service", () => {
        * check to see if the number of items returned matches the length of the filtered array
        * check to see if the filtered array has a returnProcessAt
        */
-    })
+      const productStates = []
 
-    it("sets inventoryStatus of relevant physical product to non reservable if")
+      for (let physicalProduct of physicalProducts) {
+        productStates.push({
+          productUID: physicalProduct.seasonsUID,
+          returned: true,
+          productStatus: "Dirty",
+          notes: "no notes needed here",
+        })
+      }
+      await resPhysProdService.returnMultiItems({
+        productStates,
+        droppedOffBy: "UPS",
+        trackingNumber: reservation.returnPackage.trackingNumber,
+      })
+
+      const reservationPhysProds = await prisma.client.reservationPhysicalProduct.findMany(
+        {
+          where: {
+            reservationId: reservation.id,
+          },
+          select: {
+            hasReturnProcessed: true,
+            returnProcessedAt: true,
+          },
+        }
+      )
+
+      for (const reservationPhysProd of reservationPhysProds) {
+        expect(reservationPhysProd.hasReturnProcessed).toBe(true)
+        expect(!!reservationPhysProd.returnProcessedAt).toBe(true)
+      }
+    })
   })
 })
 
