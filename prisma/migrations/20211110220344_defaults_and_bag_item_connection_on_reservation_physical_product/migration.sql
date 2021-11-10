c0bbd227b5cf6087b920a5603c83d084469621c5
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-11-10 17:04:19 -0500

    add migration to connect reservation physical product to bag item

---
+++ prisma/migrations/20211110220344_defaults_and_bag_item_connection_on_reservation_physical_product/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `position` on the `Image` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Image" DROP COLUMN "position";
