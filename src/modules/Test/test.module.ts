046a46e3104c652292c3bcdecdb4742076ed2815
Oliver <reviloz223@gmail.com>
2021-11-15 13:57:44 -0500

    remove import from test.module

--- src/modules/Test/test.module.ts
+++ src/modules/Test/test.module.ts
@@ -2,7 +2,6 @@ import { ReservationModule } from "@modules/Reservation/reservation.module"
 import { Module } from "@nestjs/common"
 import { PrismaModule } from "@prisma1/prisma.module"
 
-import { ReserveService } from "../Reservation/services/reserve.service"
 import { UtilsModule } from "../Utils/utils.module"
 import { TestUtilsService } from "./services/test.service"
 
