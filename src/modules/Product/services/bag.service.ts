2de865435c23a567a324310a425799a4d7412cc6
Kieran Gillen <kgillen@gmail.com>
2021-11-16 19:54:46 +0100

    Updates received to processing

--- src/modules/Product/services/bag.service.ts
+++ src/modules/Product/services/bag.service.ts
@@ -36,7 +36,7 @@ enum BagSectionStatus {
   // Added sections: These combine multiple other statuses
   Inbound = "Inbound",
   Outbound = "Outbound",
-  Received = "Received",
+  Processing = "Processing",
 }
 
 @Injectable()
@@ -146,7 +146,7 @@ export class BagService {
         BagSectionStatus.ReturnPending,
 
         // Outbound
-        BagSectionStatus.Received,
+        BagSectionStatus.Processing,
         BagSectionStatus.InTransitOutbound,
         BagSectionStatus.DeliveredToCustomer,
 
@@ -796,7 +796,7 @@ export class BagService {
         deliveryStatusText = "Shipped"
         deliveryTrackingUrl = this.getTrackingUrl(filteredBagItems, "inbound")
         break
-      case "Received":
+      case "Processing":
         // 1. Outbound step 1
         filteredBagItems = bagItems.filter(item => {
           const status = item.reservationPhysicalProduct?.status
