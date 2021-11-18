f429bedbcd5fca853d280e3500e93b5d9ee2ce87
Luc Succes <l2succes@gmail.com>
2021-11-18 18:35:44 -0500

    Adds cron job to restart crashed dynos

---
+++ src/modules/Cron/services/heroku.job.service.ts
@@ -0,0 +1,75 @@
+import { Injectable, Logger } from "@nestjs/common"
+import { Cron, CronExpression } from "@nestjs/schedule"
+import fetch from "node-fetch"
+
+@Injectable()
+export class HerokuJobs {
+  private readonly logger = new Logger(`Cron: ${HerokuJobs.name}`)
+
+  constructor() {}
+
+  token: string = null
+
+  @Cron(CronExpression.EVERY_MINUTE)
+  async restartDynos() {
+    await this.checkIfDynoNeedsToBeRestarted()
+  }
+
+  async authenticateWithHeroku() {
+    const res = await fetch("https://api.heroku.com/oauth/authorizations", {
+      method: "POST",
+      body: JSON.stringify({
+        description: "sample description",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${process.env.HEROKU_AUTH}`,
+        Accept: "application/vnd.heroku+json; version=3",
+      },
+    })
+    return res.json()
+  }
+
+  async getDynos(app) {
+    const dynos = await fetch(`https://api.heroku.com/apps/${app}/dynos`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/vnd.heroku+json; version=3",
+        Authorization: `Bearer ${this.token}`,
+      },
+    }).then(res => res.json())
+
+    for (const dyno of dynos) {
+      const { name, state } = dyno
+      this.logger.log(`${name} is ${state}`)
+    }
+
+    const downDynos = dynos.filter(dyno => dyno.state === "down")
+
+    const messages = []
+
+    for (let downDyno of downDynos) {
+      messages.push(`${downDyno.type} ${downDyno.name} is down 🔴`)
+      await fetch(
+        `https://api.heroku.com/apps/${app}/dynos/${downDyno.name}/restart`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/vnd.heroku+json; version=3",
+            Authorization: `Bearer ${this.token}`,
+          },
+        }
+      )
+      this.logger.log(`♺ Restarting ${downDyno.name}...`)
+    }
+  }
+
+  async checkIfDynoNeedsToBeRestarted() {
+    const tokenData = await this.authenticateWithHeroku()
+    const { access_token } = tokenData
+
+    this.token = access_token.token
+
+    await this.getDynos("monsoon-production")
+  }
+}
