1691846294ea3eb8af5b2518bbd4f34ce3997180
Oliver <reviloz223@gmail.com>
2021-12-14 17:58:15 -0500

    make query for rentalInvoices that have no rpps

--- src/scripts/dontDelete/runQuery.ts
+++ src/scripts/dontDelete/runQuery.ts
@@ -1,13 +1,46 @@
 import "module-alias/register"
 
+import { UniqueArgumentNames } from "graphql/validation/rules/UniqueArgumentNames"
+import { uniq } from "lodash"
+
 import { PrismaService } from "../../prisma/prisma.service"
 
 const run = async () => {
   const ps = new PrismaService()
 
-  await ps.client.reservationPhysicalProduct.deleteMany({
-    where: { physicalProduct: { seasonsUID: "CRGR-BLK-SS-002-02" } },
+  const rpps = await ps.client.reservationPhysicalProduct.findMany({
+    where: {
+      status: {
+        notIn: ["Lost", "Cancelled", "ReturnProcessed"],
+      },
+    },
   })
-  console.log("done")
+
+  const resIds = uniq(rpps.map(a => a.reservationId))
+
+  const rentalInvoices = await ps.client.rentalInvoice.findMany({
+    where: {
+      status: "Draft",
+      reservations: {
+        some: {
+          id: {
+            in: resIds,
+          },
+        },
+      },
+    },
+    select: {
+      id: true,
+      reservationPhysicalProducts: true,
+      billingEndAt: true,
+    },
+  })
+
+  const noRPPRentalInvoices = rentalInvoices.filter(
+    a => a.reservationPhysicalProducts.length === 0
+  )
+  console.dir(noRPPRentalInvoices, { depth: null })
+  console.log(noRPPRentalInvoices.length)
 }
+
 run()
