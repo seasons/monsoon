0d8043714a93a2955cf923e00b7d83b3106bbbf6
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-12-13 19:01:53 -0500

    misc script updates

--- src/scripts/dontDelete/runQuery.ts
+++ src/scripts/dontDelete/runQuery.ts
@@ -5,8 +5,13 @@ import { PrismaService } from "../../prisma/prisma.service"
 const run = async () => {
   const ps = new PrismaService()
 
-  await ps.client.reservationPhysicalProduct.deleteMany({
-    where: { physicalProduct: { seasonsUID: "CRGR-BLK-SS-002-02" } },
+  await ps.client.rentalInvoice.create({
+    data: {
+      status: "Draft",
+      billingStartAt: new Date(),
+      billingEndAt: new Date(2021, 11, 26),
+      membership: { connect: { id: "cku2yrfmt325712euj5safcueo" } },
+    },
   })
   console.log("done")
 }
