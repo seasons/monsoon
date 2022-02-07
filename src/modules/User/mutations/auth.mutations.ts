965b30d74dc291c06d000637cfd8bd9f2cdd20bc
Faiyam Rahman <faiyam@faiyamrahman.com>
2022-02-07 15:18:40 -0500

    deprecate signup mutation

--- src/modules/User/mutations/auth.mutations.ts
+++ src/modules/User/mutations/auth.mutations.ts
@@ -1,11 +1,7 @@
 import { User } from "@app/decorators"
-import { Application } from "@app/decorators/application.decorator"
-import { Select } from "@app/decorators/select.decorator"
-import { SegmentService } from "@app/modules/Analytics/services/segment.service"
-import { QueryUtilsService } from "@app/modules/Utils/services/queryUtils.service"
 import { UtilsService } from "@app/modules/Utils/services/utils.service"
 import { Args, Info, Mutation, Resolver } from "@nestjs/graphql"
-import { pick } from "lodash"
+import { ApolloError } from "apollo-server"
 
 import { AuthService } from "../services/auth.service"
 
@@ -13,7 +9,6 @@ import { AuthService } from "../services/auth.service"
 export class AuthMutationsResolver {
   constructor(
     private readonly auth: AuthService,
-    private readonly segment: SegmentService,
     private readonly utils: UtilsService
   ) {}
 
@@ -34,65 +29,8 @@ export class AuthMutationsResolver {
   }
 
   @Mutation()
-  async signup(
-    @Args()
-    { email, password, firstName, lastName, details, referrerId, giftId, utm },
-    @Application() application,
-    @Info() info
-  ) {
-    const select = {
-      ...this.utils.getSelectFromInfoAt(info, "customer"),
-      ...this.utils.getSelectFromInfoAt(info, "user"),
-    }
-
-    const { user, tokenData, customer } = await this.auth.signupUser({
-      email: email.toLowerCase(),
-      password,
-      firstName,
-      lastName,
-      details,
-      referrerId,
-      utm,
-      select,
-      giftId,
-    })
-
-    // Add them to segment and track their account creation event
-    const now = new Date()
-    this.segment.identify(user.id, {
-      ...this.auth.extractSegmentReservedTraitsFromCustomerDetail({
-        ...details,
-        ...customer.detail,
-      }),
-      ...pick(user, [
-        "firstName",
-        "lastName",
-        "id",
-        "roles",
-        "email",
-        "auth0Id",
-      ]),
-      ...this.utils.formatUTMForSegment(utm),
-      createdAt: now.toISOString(),
-    })
-
-    this.segment.track(user.id, "Created Account", {
-      name: `${user.firstName} ${user.lastName}`,
-      ...pick(user, ["firstName", "lastName", "email"]),
-      customerID: customer.id,
-      application,
-      ...this.utils.formatUTMForSegment(utm),
-      impactId: details?.impactId,
-      discoveryReference: details?.discoveryReference,
-    })
-
-    return {
-      token: tokenData.access_token,
-      refreshToken: tokenData.refresh_token,
-      expiresIn: tokenData.expires_in,
-      user,
-      customer,
-    }
+  async signup() {
+    throw new ApolloError("Signup is not available anymore")
   }
 
   @Mutation()
